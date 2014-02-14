e5773d325453913cdbc6369e889212ba2034dab4
Nick Arnold <nicka@labkey.com>
2014-02-13 18:44:12 -0800

    Group listing in update group.

--- webapp/Connector/src/view/GroupSave.js
+++ webapp/Connector/src/view/GroupSave.js
@@ -68,6 +68,7 @@ Ext.define('Connector.view.GroupSave', {
             this.createGroup = Ext.create('Ext.Container', {
                 hidden: this.mode !== Connector.view.GroupSave.modes.CREATE,
                 activeMode: Connector.view.GroupSave.modes.CREATE,
+                style: 'padding-top: 10px;',
                 items: [{
                     itemId: 'creategroupform',
                     xtype: 'form',
@@ -78,10 +79,12 @@ Ext.define('Connector.view.GroupSave', {
                     },
                     items: [{
                         xtype: 'textfield',
+                        itemId: 'groupname',
                         name: 'groupname',
                         emptyText: 'Enter a group name',
                         height: 30,
-                        allowBlank: false
+                        allowBlank: false,
+                        validateOnBlur: false
                     },{
                         xtype: 'textareafield',
                         name: 'groupdescription',
@@ -90,6 +93,7 @@ Ext.define('Connector.view.GroupSave', {
                         xtype: 'radiogroup',
                         columns: 1,
                         allowBlank: false,
+                        validateOnBlur: false,
                         items: [
                             { boxLabel: 'Live Filters: Update group with new data', name: 'groupselect', inputValue: 'live', checked: true},
                             { boxLabel: 'Snapshot: Keep this group static', name: 'groupselect', inputValue: 'static' }
@@ -127,6 +131,15 @@ Ext.define('Connector.view.GroupSave', {
                         ui: 'rounded-inverted-accent'
                     }]
                 }],
+                listeners : {
+                    afterrender : {
+                        fn: function(c) {
+                            c.getComponent('creategroupform').getComponent('groupname').focus(false, true);
+                        },
+                        single: true,
+                        scope: this
+                    }
+                },
                 scope: this
             });
         }
@@ -135,22 +148,66 @@ Ext.define('Connector.view.GroupSave', {
     },
 
     getReplaceGroup : function() {
+
+        // models Participant Groups and Cohorts mixed
+        Ext.define('LABKEY.study.GroupCohort', {
+            extend : 'Ext.data.Model',
+            fields : [
+                {name : 'id'},
+                {name : 'label'},
+                {name : 'description'},
+                {name : 'filters'},
+                {name : 'type'}
+            ]
+        });
+
         if (!this.replaceGroup) {
             this.replaceGroup = Ext.create('Ext.Container', {
                 hidden: this.mode !== Connector.view.GroupSave.modes.REPLACE,
                 activeMode: Connector.view.GroupSave.modes.REPLACE,
+                style: 'padding-top: 10px;',
                 items: [{
+                    xtype: 'box',
+                    autoEl: {
+                        tag: 'div',
+                        style: 'padding: 5px 0; font-family: Arial; font-size: 11pt;',
+                        html: 'My groups:'
+                    }
+                },{
+                    xtype: 'container',
+                    height: 70,
+                    overflowY: 'auto',
+                    style: 'border: 1px solid lightgrey;',
+                    items: [{
+                        xtype: 'groupsavelistview',
+                        listeners: {
+                            select: function(sm, group) {
+                                this.setActive(group);
+                            },
+                            scope: this
+                        },
+                        scope: this
+                    }]
+                },{
                     itemId: 'creategroupform',
                     xtype: 'form',
                     ui: 'custom',
                     width: '100%',
+                    style: 'padding-top: 10px;',
                     defaults: {
                         width: '100%'
                     },
                     items: [{
+                        xtype: 'box',
+                        autoEl: {
+                            tag: 'div',
+                            style: 'padding: 5px 0; font-family: Arial; font-size: 11pt;',
+                            html: 'Description:'
+                        }
+                    },{
                         xtype: 'textareafield',
-                        name: 'groupdescription',
-                        emptyText: 'Group description'
+                        itemId: 'groupdescription',
+                        name: 'groupdescription'
                     },{
                         xtype: 'radiogroup',
                         columns: 1,
@@ -221,6 +278,17 @@ Ext.define('Connector.view.GroupSave', {
         return this.mode;
     },
 
+    setActive : function(groupModel) {
+        var form = this.replaceGroup.getComponent('creategroupform');
+        if (form) {
+            var description = form.getComponent('groupdescription');
+            var d = groupModel.get('description');
+            console.log(groupModel.get('label'));
+            console.log(d);
+            description.setValue(d);
+        }
+    },
+
     clearForm : function() {
         if (this.form) {
             this.form.getForm().reset();
@@ -245,5 +313,77 @@ Ext.define('Connector.view.GroupSave', {
         if (errorEl) {
             errorEl.hide();
         }
+    }
+});
+
+Ext.define('Connector.view.GroupSaveList', {
+
+    extend: 'Ext.view.View',
+
+    alias: 'widget.groupsavelistview',
+
+    trackOver: true,
+
+    emptyText: '<div class="emptytext"><span class="left-label">No groups defined</span>',
+
+    overItemCls: 'save-label-over',
+
+    selectedItemCls: 'save-label-selected',
+
+    itemSelector: 'div.save-label',
+
+    tpl: new Ext.XTemplate(
+            '<tpl for=".">',
+                '<div class="save-label">{label}</div>',
+            '</tpl>'
+    ),
+
+    initComponent : function() {
+
+        // models Participant Groups and Cohorts mixed
+        if (!Ext4.ModelManager.isRegistered('LABKEY.study.GroupCohort')) {
+            Ext.define('LABKEY.study.GroupCohort', {
+                extend : 'Ext.data.Model',
+                fields : [
+                    {name : 'id'},
+                    {name : 'label'},
+                    {name : 'description'},
+                    {name : 'filters'},
+                    {name : 'type'}
+                ]
+            });
+        }
+
+        var storeConfig = {
+            pageSize : 100,
+            model    : 'LABKEY.study.GroupCohort',
+            autoLoad : true,
+            proxy    : {
+                type   : 'ajax',
+                url    : LABKEY.ActionURL.buildURL('participant-group', 'browseParticipantGroups.api'),
+                reader : {
+                    type : 'json',
+                    root : 'groups'
+                }
+            },
+            listeners : {
+                load : function(s, recs) {
+                    for (var i=0; i < recs.length; i++)
+                    {
+                        if (recs[i].data.id < 0)
+                            s.remove(recs[i]);
+                    }
+                }
+            }
+        };
+
+        var groupConfig = Ext.clone(storeConfig);
+        Ext.apply(groupConfig.proxy, {
+            extraParams : { type : 'participantGroup'}
+        });
+
+        this.store = Ext.create('Ext.data.Store', groupConfig);
+
+        this.callParent();
     }
 });
