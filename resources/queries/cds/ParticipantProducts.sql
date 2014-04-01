9947594ee1fb0100fe21393dfe6549267b1e0ae8
labkey-marki <marki@labkey.com>
2014-03-31 17:10:21 -0700

    Remove no-longer used ParticipantProducts sql query

--- resources/queries/cds/ParticipantProducts.sql
+++
@@ -1,4 +0,0 @@
-SELECT pt.SubjectId, p.*
-FROM study.ParticipantTreatments pt
-     JOIN study.TreatmentProductMap tpm on pt.TreatmentId=tpm.TreatmentId
-     JOIN study.Product p on p.RowId=tpm.ProductId
