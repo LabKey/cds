20dfaa7ef392044f0ab9b7f8ec67410c5fee5385
Sam Baker <sam@ag-1139-mac.local>
2014-04-22 16:39:06 -0700

    Replace 4-column layout test for Study Products page with more realistic 2-column layout, with lorem ipsum text.

--- webapp/Connector/cube.js
+++ webapp/Connector/cube.js
@@ -84,32 +84,20 @@ Ext4.define('Connector.cube.Configuration', {
                 detailItemModules: [[{
                     type: 'text',
                     staticData: {
-                        title: 'Test Module',
-                        text: 'Module 1, column 1'
+                        title: 'Placeholder',
+                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                     }
                 }, { 
                     type: 'text',
                     staticData: {
-                        title: 'Test Module',
-                        text: 'Module 2, column 1'
+                        title: 'Placeholder',
+                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                     }
                 }], [{
                     type: 'text',
                     staticData: {
-                        title: 'Test Module',
-                        text: 'Module 1, column 2'
-                    }
-                }], [{
-                    type: 'text',
-                    staticData: {
-                        title: 'Test Module',
-                        text: 'Module 1, column 3'
-                    }
-                }], [{
-                    type: 'text',
-                    staticData: {
-                        title: 'Test Module',
-                        text: 'Module 1, column 4'
+                        title: 'Placeholder',
+                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                     }
                 }]],
 
