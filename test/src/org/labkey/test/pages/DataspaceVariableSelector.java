14ba96b158556dda710035d190a6e9c13ea94371
tchad <tchad@labkey.com>
2014-04-21 20:10:15 -0700

    Refactor CDSTest to move variable selector helpers into separate classes. Remove CDS specific helpers from ExtHelper

---
+++ test/src/org/labkey/test/pages/DataspaceVariableSelector.java
@@ -0,0 +1,69 @@
+package org.labkey.test.pages;
+
+import org.labkey.test.BaseWebDriverTest;
+import org.labkey.test.Locator;
+import org.labkey.test.tests.CDSTest;
+import org.labkey.test.util.Ext4Helper;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public abstract class DataspaceVariableSelector
+{
+    protected BaseWebDriverTest _test;
+
+    public DataspaceVariableSelector(BaseWebDriverTest test)
+    {
+        _test = test;
+    }
+
+    protected abstract String getPickerClass();
+    protected abstract Locator getOpenButton();
+
+    public void openSelectorWindow()
+    {
+        WebElement openButton = _test.shortWait().until(ExpectedConditions.elementToBeClickable(getOpenButton().toBy()));
+
+        openButton.click();
+
+        _test.shortWait().until(ExpectedConditions.elementToBeClickable(sourcePanelRow().toBy()));
+    }
+
+    protected Locator.CssLocator pickerPanel()
+    {
+        return Locator.css("." + getPickerClass());
+    }
+
+    protected Locator.CssLocator sourcePanelRow()
+    {
+        return pickerPanel().append(".sourcepanel div.itemrow span.val");
+    }
+
+    public void pickSource(String source)
+    {
+        _test.waitAndClick(sourcePanelRow().withText(source));
+    }
+
+    //Pick measure from one of multiple split panel measure pickers
+    public void pickMeasure(String source, String measure, boolean isMultiSelect, boolean keepSelection)
+    {
+        pickSource(source);
+        //select measure
+        if (isMultiSelect)
+        {
+            Locator.CssLocator _variablePanelRow = pickerPanel().append(".measuresgrid ." + Ext4Helper.getCssPrefix() + "grid-row");
+            _test.shortWait().until(ExpectedConditions.elementToBeClickable(_variablePanelRow.toBy())); // if one row is ready, all should be
+            _test._extHelper.selectExt4GridItem("label", measure, -1, getPickerClass() + " .measuresgrid", keepSelection);
+        }
+        else
+        {
+            Locator.CssLocator _variablePanelRow = pickerPanel().append(".measuresgrid div.itemrow");
+            _test.shortWait().until(ExpectedConditions.elementToBeClickable(_variablePanelRow.toBy())); // if one row is ready, all should be
+            _test.click(_variablePanelRow.withText(measure));
+        }
+    }
+
+    public void pickMeasure(String source, String measure)
+    {
+        pickMeasure(source, measure, false, false);
+    }
+}
