deb08527493130acfca094d271f3c186ba0cd219
Sam Baker <sam@ag-1139-mac.local>
2014-05-20 13:32:21 -0700

    Remove test popup content

--- webapp/Connector/src/app/view/module/StudyProducts.js
+++ webapp/Connector/src/app/view/module/StudyProducts.js
@@ -18,7 +18,7 @@ Ext.define('Connector.view.module.StudyProducts', {
             '<tpl for="items">',
                 '<div class="item-row">',
                     '<p><a href="#learn/learn/vaccine/{[encodeURIComponent(values)]}">{.}</a></p>',
-                    '<p><a class="popupLink">{.} (popup)</a></p>',
+//                    '<p><a class="popupLink">{.} (popup)</a></p>',
                 '</div>',
             '</tpl>',
         '</p></tpl>'),
@@ -46,26 +46,26 @@ Ext.define('Connector.view.module.StudyProducts', {
                 data.items = set;
                 this.update(data);
 
-                var links = this.getEl().query('a[cls~=popupLink]');
-                links = Ext.select('.popupLink', this.getEl().dom);
-                links.on('click', function(a,b,c) {
-                    var target = Ext.get(b);
-                    var container = target.up('.modulecontainer');
-                    var content = Connector.factory.Module.defineView({
-                        type: 'text',
-                        staticData: {
-                            title: "Popup test",
-                            text: "From thousands of spreadsheets scattered across computers to huge files generated in genomics and proteomics experiments, research teams face daunting data management challenges. LabKey Server is a secure, web-based data integration platform that can be customized to meet the evolving needs of translational research organizations."
-                        }
-                    }, this.model, this.state);
+                // var links = this.getEl().query('a[cls~=popupLink]');
+                // links = Ext.select('.popupLink', this.getEl().dom);
+                // links.on('click', function(a,b,c) {
+                //     var target = Ext.get(b);
+                //     var container = target.up('.modulecontainer');
+                //     var content = Connector.factory.Module.defineView({
+                //         type: 'text',
+                //         staticData: {
+                //             title: "Popup",
+                //             text: "From thousands of spreadsheets scattered across computers to huge files generated in genomics and proteomics experiments, research teams face daunting data management challenges. LabKey Server is a secure, web-based data integration platform that can be customized to meet the evolving needs of translational research organizations."
+                //         }
+                //     }, this.model, this.state);
 
-                    var popup = Ext.create('Connector.view.Popup', {
-                        container: container,
-                        anchor: target,
-                        content: content,
-                        width: 350
-                    });
-                }, this);
+                //     var popup = Ext.create('Connector.view.Popup', {
+                //         container: container,
+                //         anchor: target,
+                //         content: content,
+                //         width: 350
+                //     });
+                // }, this);
             },
             scope: this
         };
