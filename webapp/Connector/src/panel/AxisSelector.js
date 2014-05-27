d7830165213e57d71f346cb6effa1f799817477e
Alan <alanv@labkey.com>
2014-05-27 13:14:38 -0700

    Do not show linear/log option for categorical measures

--- webapp/Connector/src/panel/AxisSelector.js
+++ webapp/Connector/src/panel/AxisSelector.js
@@ -251,7 +251,7 @@ Ext.define('Connector.panel.AxisSelector', {
         {
             this.getSelectionDisplay().setMeasureSelection(measure);
             this.getSelectionDisplay().setVariableOptions(measure, this.selectedSource);
-            this.getSelectionDisplay().getScaleForm().setVisible(measure.get('variableType') == null);
+            this.getSelectionDisplay().getScaleForm().setVisible(measure.get('variableType') == null && measure.get('type') !== 'VARCHAR');
             this.lastMeasure = measure;
         }
 
