481bb931df190de1c77099ac556b086396d7e020
Alan <alanv@labkey.com>
2014-05-29 14:06:48 -0700

    Add test cases for study axis

--- test/src/org/labkey/test/tests/CDSTest.java
+++ test/src/org/labkey/test/tests/CDSTest.java
@@ -58,6 +58,7 @@ public class CDSTest extends BaseWebDriverTest implements PostgresOnlyTest
     private static final String GROUP_LIVE_FILTER = "CDSTest_DGroup";
     private static final String GROUP_STATIC_FILTER = "CDSTest_EGroup";
     private static final String STUDY_GROUP = "Study Group Verify";
+    private static final String[] DESIRED_STUDIES = {"DemoSubset", "NotCHAVI001", "NotCHAVI008", "NotRV144"};
     
     private final CDSHelper cds = new CDSHelper(this);
     private final CDSAsserts _asserts = new CDSAsserts(this);
@@ -93,6 +94,7 @@ public class CDSTest extends BaseWebDriverTest implements PostgresOnlyTest
 
         initTest.doCleanup(false);
         CDSInitializer _initializer = new CDSInitializer(initTest, initTest.getProjectName());
+        _initializer.setDesiredStudies(DESIRED_STUDIES);
         _initializer.setupDataspace();
 
         currentTest = initTest;
