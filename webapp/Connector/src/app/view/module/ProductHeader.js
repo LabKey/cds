d7a04b237850acd3f5c952000a49d7f4ffef4e71
Sam Baker <sam@artefactgroup.com>
2014-06-13 17:09:08 -0700

    Pluralize inserts, add style for learn about filter

--- webapp/Connector/src/app/view/module/ProductHeader.js
+++ webapp/Connector/src/app/view/module/ProductHeader.js
@@ -14,7 +14,7 @@ Ext.define('Connector.view.module.ProductHeader', {
             '<tpl if="model.get(\'Type\')"><p class="item-row">Product Type: {[values.model.get("Type")]}</p></tpl>',
             '<tpl if="model.get(\'Class\')"><p class="item-row">Class: {[values.model.get("Class")]}</p></tpl>',
             '<tpl if="model.get(\'Subclass\')"><p class="item-row">Subclass: {[values.model.get("Subclass")]}</p></tpl>',
-            '<tpl if="model.get(\'Inserts\')"><p class="item-row">Insert: {[values.model.get("Inserts")]}</p></tpl>',
+            '<tpl if="model.get(\'Inserts\')"><p class="item-row">Inserts: {[values.model.get("Inserts")]}</p></tpl>',
         '</tpl>',
     {
         typeString : function(model) {
