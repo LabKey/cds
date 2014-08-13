a008659eb426d4fd3088005240282beac32c6319
cnathe <cnathe@labkey.com>
2014-08-13 10:06:21 -0500

    Add some comments to the LoadingMask.js

--- webapp/Connector/src/plugin/LoadingMask.js
+++ webapp/Connector/src/plugin/LoadingMask.js
@@ -42,12 +42,14 @@ Ext.define('Connector.plugin.LoadingMask', {
             hideMask: this.hideMask
         });
 
+        // attach the begin events to the specified component to show mask
         if (this.beginConfig && this.beginConfig.component && Ext.isArray(this.beginConfig.events)) {
             Ext.each(this.beginConfig.events, function(eventName){
                 this.beginConfig.component.on(eventName, component.showMask, component);
             }, this);
         }
 
+        // attach the end events to the specified component to hide mask
         if (this.endConfig && this.endConfig.component && Ext.isArray(this.endConfig.events)) {
             Ext.each(this.endConfig.events, function(eventName){
                 this.endConfig.component.on(eventName, component.hideMask, component);
