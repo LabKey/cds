f1d6e46ddd7b4fea0dafb483a62e8b66089c66d8
Nick Arnold <nicka@labkey.com>
2014-11-18 09:06:57 -0800

    Remove many unused/deprecated actions.

--- src/org/labkey/cds/CDSController.java
+++ src/org/labkey/cds/CDSController.java
@@ -18,23 +18,15 @@ package org.labkey.cds;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.labkey.api.action.Action;
 import org.labkey.api.action.ActionType;
 import org.labkey.api.action.ApiAction;
-import org.labkey.api.action.ApiJsonWriter;
 import org.labkey.api.action.ApiResponse;
-import org.labkey.api.action.ApiResponseWriter;
 import org.labkey.api.action.ApiSimpleResponse;
-import org.labkey.api.action.ApiVersion;
 import org.labkey.api.action.BaseViewAction;
-import org.labkey.api.action.CustomApiForm;
-import org.labkey.api.action.ExtendedApiQueryResponse;
 import org.labkey.api.action.FormViewAction;
 import org.labkey.api.action.HasBindParameters;
 import org.labkey.api.action.Marshal;
@@ -43,24 +35,14 @@ import org.labkey.api.action.MutatingApiAction;
 import org.labkey.api.action.NullSafeBindException;
 import org.labkey.api.action.SimpleViewAction;
 import org.labkey.api.action.SpringActionController;
-import org.labkey.api.cache.Cache;
-import org.labkey.api.cache.CacheLoader;
-import org.labkey.api.cache.CacheManager;
 import org.labkey.api.collections.CaseInsensitiveHashMap;
 import org.labkey.api.data.Container;
-import org.labkey.api.data.DataRegion;
 import org.labkey.api.data.DisplayColumn;
 import org.labkey.api.data.ExcelWriter;
 import org.labkey.api.data.PropertyManager;
-import org.labkey.api.data.RuntimeSQLException;
-import org.labkey.api.data.ShowRows;
-import org.labkey.api.data.Table;
-import org.labkey.api.files.FileContentService;
 import org.labkey.api.query.QueryForm;
 import org.labkey.api.query.QueryService;
-import org.labkey.api.query.QuerySettings;
 import org.labkey.api.query.QueryView;
-import org.labkey.api.query.TempQuerySettings;
 import org.labkey.api.query.UserSchema;
 import org.labkey.api.rss.RSSFeed;
 import org.labkey.api.rss.RSSService;
@@ -70,19 +52,14 @@ import org.labkey.api.security.RequiresNoPermission;
 import org.labkey.api.security.RequiresPermissionClass;
 import org.labkey.api.security.permissions.AdminPermission;
 import org.labkey.api.security.permissions.ReadPermission;
-import org.labkey.api.services.ServiceRegistry;
 import org.labkey.api.study.DataSet;
 import org.labkey.api.study.StudyService;
-import org.labkey.api.util.Compress;
-import org.labkey.api.util.PageFlowUtil;
-import org.labkey.api.util.ResultSetUtil;
 import org.labkey.api.util.URLHelper;
 import org.labkey.api.view.ActionURL;
 import org.labkey.api.view.HtmlView;
 import org.labkey.api.view.HttpView;
 import org.labkey.api.view.JspView;
 import org.labkey.api.view.NavTree;
-import org.labkey.api.view.ViewContext;
 import org.labkey.api.view.template.PageConfig;
 import org.labkey.cds.model.Citable;
 import org.labkey.cds.model.CitableAuthor;
@@ -94,11 +71,6 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
-import java.io.StringWriter;
-import java.lang.Object;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -499,198 +471,6 @@ public class CDSController extends SpringActionController
         }
     }
 
-    public static class ExecuteSqlForm
-    {
-        private String _sql;
-        private Integer _maxRows;
-        private Integer _offset;
-        private boolean _saveInSession;
-        private boolean _includeTotalCount = true;
-
-        public String getSql()
-        {
-            return _sql;
-        }
-
-        public void setSql(String sql)
-        {
-            _sql = sql;
-        }
-
-        public Integer getMaxRows()
-        {
-            return _maxRows;
-        }
-
-        public void setMaxRows(Integer maxRows)
-        {
-            _maxRows = maxRows;
-        }
-
-        public Integer getOffset()
-        {
-            return _offset;
-        }
-
-        public void setOffset(Integer offset)
-        {
-            _offset = offset;
-        }
-
-        public void setLimit(Integer limit)
-        {
-            _maxRows = limit;
-        }
-
-        public void setStart(Integer start)
-        {
-            _offset = start;
-        }
-
-        public boolean isSaveInSession()
-        {
-            return _saveInSession;
-        }
-
-        public void setSaveInSession(boolean saveInSession)
-        {
-            _saveInSession = saveInSession;
-        }
-
-        public boolean isIncludeTotalCount()
-        {
-            return _includeTotalCount;
-        }
-
-        public void setIncludeTotalCount(boolean includeTotalCount)
-        {
-            _includeTotalCount = includeTotalCount;
-        }
-
-        // this holds all our context for SQLCacheLoader, so we need to pass in a little context here
-        ViewContext _context;
-
-        ViewContext getViewContext()
-        {
-            return _context;
-        }
-
-        void setViewContext(ViewContext context)
-        {
-            _context = context;
-        }
-
-        BindException _errors;
-
-        public BindException getErrors()
-        {
-            return _errors;
-        }
-
-        public void setErrors(BindException errors)
-        {
-            _errors = errors;
-        }
-
-    }
-
-
-    class SQLCacheLoader implements CacheLoader<String,byte[]>
-    {
-        @Override
-        public byte[] load(String key, @Nullable Object argument)
-        {
-            ExecuteSqlForm form = (ExecuteSqlForm)argument;
-
-            String sql = StringUtils.trimToNull(form.getSql());
-            if (null == sql)
-                throw new IllegalArgumentException("No value was supplied for the required parameter 'sql'.");
-
-            ViewContext context = form.getViewContext();
-            BindException errors = form.getErrors();
-
-            //create a temp query settings object initialized with the posted LabKey SQL
-            //this will provide a temporary QueryDefinition to Query
-            QuerySettings settings = new TempQuerySettings(context, sql);
-
-            //need to explicitly turn off various UI options that will try to refer to the
-            //current URL and query string
-            settings.setAllowChooseView(false);
-            settings.setAllowCustomizeView(false);
-
-            // Issue 12233: add implicit maxRows=100k when using client API
-            settings.setShowRows(ShowRows.PAGINATED);
-            settings.setMaxRows(100000);
-
-            //apply optional settings (maxRows, offset)
-            boolean metaDataOnly = false;
-            if (null != form.getMaxRows() && form.getMaxRows().intValue() >= 0)
-            {
-                settings.setShowRows(ShowRows.PAGINATED);
-                settings.setMaxRows(Table.ALL_ROWS == form.getMaxRows() ? 1 : form.getMaxRows());
-                metaDataOnly = (Table.ALL_ROWS == form.getMaxRows());
-            }
-
-            int offset = 0;
-            if (null != form.getOffset())
-            {
-                settings.setOffset(form.getOffset().longValue());
-                offset = form.getOffset();
-            }
-
-            //build a query view using the schema and settings
-            CDSUserSchema schema = new CDSUserSchema(context.getUser(), context.getContainer(), true);
-            QueryView view = new QueryView(schema, settings, errors);
-            view.setShowRecordSelectors(false);
-            view.setShowExportButtons(false);
-            view.setButtonBarPosition(DataRegion.ButtonBarPosition.NONE);
-            view.setShowPagination(form.isIncludeTotalCount());
-
-            boolean isEditable = false;
-
-            try
-            {
-                ExtendedApiQueryResponse res = new ExtendedApiQueryResponse(view, isEditable,
-                        false, "cds", form.isSaveInSession() ? settings.getQueryName() : "sql", offset, null, metaDataOnly, false, false);
-                StringWriter sw = new StringWriter();
-                res.render(new ApiJsonWriter(sw));
-                return Compress.compressGzip(sw.toString());
-            }
-            catch (Exception x)
-            {
-                errors.reject(ERROR_MSG, x.getMessage());
-                return null;
-            }
-        }
-    }
-
-
-    // containerid:SQL
-    static Cache<String,byte[]> resultsCache = CacheManager.getBlockingCache(1000, CacheManager.DAY, "CDS results cache", null);
-
-    @RequiresPermissionClass(ReadPermission.class)
-    @ApiVersion(9.1)
-    public class ExecuteSqlAction extends ApiAction<ExecuteSqlForm>
-    {
-
-        public ApiResponse execute(ExecuteSqlForm form, BindException errors) throws Exception
-        {
-            String sql = StringUtils.trimToNull(form.getSql());
-            if (null == sql)
-                throw new IllegalArgumentException("No value was supplied for the required parameter 'sql'.");
-
-            form.setViewContext(getViewContext());
-            form.setErrors(errors);
-            String key = getContainer().getId() + ":" + form.getSql();
-            byte[] bytes = resultsCache.get(key, form, new SQLCacheLoader());
-
-            getViewContext().getResponse().setContentType(ApiJsonWriter.CONTENT_TYPE_JSON);
-            getViewContext().getResponse().setHeader("Content-Encoding", "gzip");
-            getViewContext().getResponse().getOutputStream().write(bytes);
-            return null;
-        }
-    }
-
 
     public static class StateForm implements HasBindParameters
     {
@@ -771,157 +551,6 @@ public class CDSController extends SpringActionController
 
 
     /***** experimenting with derby/mondrian ******/
-
-
-    public static class ExecuteMdxForm
-    {
-        String _query = null;
-
-        public String getQuery()
-        {
-            return _query;
-        }
-
-        public void setQuery(String query)
-        {
-            _query = query;
-        }
-    }
-
-
-    public static class WarmCacheForm
-    {
-        private String _query;
-
-        public String getQuery()
-        {
-            return _query;
-        }
-
-        public void setQuery(String query)
-        {
-            _query = query;
-        }
-    }
-
-    private static Thread warmCacheThread;
-    @RequiresPermissionClass(AdminPermission.class)
-    public class WarmCacheAction extends FormViewAction<WarmCacheForm>
-    {
-        @Override
-        public void validateCommand(WarmCacheForm target, Errors errors)
-        {
-
-        }
-
-        @Override
-        public ModelAndView getView(WarmCacheForm form, boolean reshow, BindException errors) throws Exception
-        {
-            if (null == form.getQuery())
-            {
-                File queryFile = getQueryFile();
-                if (queryFile.exists())
-                    form.setQuery(PageFlowUtil.getFileContentsAsString(queryFile));
-            }
-            return new JspView(CDSController.class, "view/warmCache.jsp", form, errors);
-        }
-
-        @Override
-        public boolean handlePost(WarmCacheForm form, BindException errors) throws Exception
-        {
-            String query = StringUtils.trimToNull(form.getQuery());
-            if (null != query)
-                FileUtils.write(getQueryFile(), query);
-
-            int index = 0;
-            if (null != query)
-            {
-                // TODO
-/*                OlapConnection conn = getConnection();
-                List<String> statements = getStatements(query);
-                for (String statement : statements)
-                {
-                    Throwable ex = null;
-                    OlapStatement stmt = conn.createStatement();
-                    try
-                    {
-                        index++;
-                        stmt.executeOlapQuery(statement);
-                    }
-                    catch (TokenMgrError olapx)
-                    {
-                        ex = olapx;
-                    }
-                    catch (OlapException olapx)
-                    {
-                        ex = olapx;
-                    }
-                    catch (MondrianException olapx)
-                    {
-                        ex = olapx;
-                    }
-                    while (null != ex && null != ex.getCause() && ex.getCause() != ex)
-                        ex = ex.getCause();
-
-                    if (null != ex)
-                    {
-                        errors.reject(ERROR_MSG, "Statement " + index + ": " + ex.getMessage());
-                    }
-
-                }
-*/
-            }
-
-
-            return !errors.hasErrors();
-        }
-
-        private File getQueryFile()
-        {
-            FileContentService service = ServiceRegistry.get().getService(FileContentService.class);
-            File filesLoc = service.getFileRoot(getContainer(), FileContentService.ContentType.files);
-            return new File(filesLoc, "warmCache.mdx");
-        }
-
-        private List<String> getStatements(String queries)
-        {
-            List<String> statements = new ArrayList<>();
-            StringBuilder statement = new StringBuilder();
-            String[] lines = StringUtils.split(queries, "\n");
-            for (String line : lines)
-            {
-                line = line.trim();
-                if (line.endsWith(";"))
-                {
-                    statement.append(line.substring(0, line.length() - 1));
-                    statements.add(statement.toString());
-                    statement = new StringBuilder();
-                }
-                else
-                    statement.append(line).append("\n");
-            }
-
-            if (statement.length() > 0)
-                statements.add(statement.toString());
-
-            return statements;
-
-        }
-        @Override
-        public URLHelper getSuccessURL(WarmCacheForm form)
-        {
-            ActionURL url = new ActionURL(this.getClass(), getContainer());
-            url.addParameter("success", true);
-            return url;
-        }
-
-        @Override
-        public NavTree appendNavTrail(NavTree root)
-        {
-            root.addChild("Warm Cache");
-            return root;
-        }
-    }
 
     @RequiresPermissionClass(ReadPermission.class)
     @Action(ActionType.Export)
