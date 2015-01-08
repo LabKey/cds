7eda34649aeff6189c829427eb50b50833955693
labkey-adam <adam@labkey.com>
2015-01-08 10:58:27 -0800

    Update licenses

--- webapp/Connector/src/component/ActionTitle.js
+++ webapp/Connector/src/component/ActionTitle.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2014-2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext.define('Connector.component.ActionTitle', {
     extend: 'Ext.Component',
 
