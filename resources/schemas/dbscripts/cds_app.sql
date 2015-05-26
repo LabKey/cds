aeb05a77333ad73c8e00ececf5ef880d0edbbb0f
Nick Arnold <nicka@labkey.com>
2015-05-26 15:59:33 -0700

    Spec 23105: Initial app schema definition. Study mapping query.

---
+++ resources/schemas/dbscripts/cds_app.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS cds.Study CASCADE;
+
+CREATE TABLE cds.Study (
+  study_name VARCHAR(250) NOT NULL,
+  container ENTITYID UNIQUE NOT NULL,
+  network VARCHAR(250),
+  label VARCHAR(250),
+  short_name VARCHAR(250),
+  title VARCHAR(500),
+  type VARCHAR(250),
+  status VARCHAR(250),
+  stage VARCHAR(250),
+  population VARCHAR(250),
+  species VARCHAR(250),
+  study_cohort VARCHAR(250),
+
+  first_enr_date DATE,
+  followup_complete_date DATE,
+  start_date DATE,
+  public_date DATE,
+
+  rationale TEXT,
+  description TEXT,
+  hypothesis TEXT,
+  objectives TEXT,
+  methods TEXT,
+  findings TEXT,
+  discussion TEXT,
+  context TEXT,
+
+  CONSTRAINT PK_Study PRIMARY KEY (study_name)
+);
