55d655f0bce280e3aeeca99c6f4fb762fc8ea264
Nick Arnold <nicka@labkey.com>
2015-08-21 11:06:52 -0700

    Feedback: Create link, define initial window

--- webapp/Connector/src/panel/Feedback.js
+++ webapp/Connector/src/panel/Feedback.js
@@ -5,305 +5,212 @@
  */
 Ext.define('Connector.panel.Feedback', {
 
-    extend : 'Ext.window.Window',
+    extend: 'Ext.panel.Panel',
 
-    alias : 'widget.feedback',
+    alias: 'widget.feedback',
 
-    cls : 'feedback',
+    cls: 'feedback',
 
-    yOffset : 195,
+   // border: false,
 
-    constructor : function(config) {
+    statics: {
+        displayWindow : function(animateTarget) {
+            var win = Ext.create('Ext.window.Window', {
+                ui: 'axiswindow',
+                border: true,
+                modal: true,
+                draggable: false,
+                header: false,
+                layout: {
+                    type: 'fit'
+                },
+                items: [{
+                    xtype: 'feedback',
+                    listeners: {
+                        hide: function() {
+                            win.hide(animateTarget);
+                        },
+                        scope: this
+                    }
+                }],
+                width: 300,
+                height: 500
+            });
 
-        Ext.applyIf(config, {
-            border : false,
-            frame  : false,
-            ui     : 'custom',
-            autoShow : true,
-            closable : false,
-            resizable : false,
-            shadow : false
-        });
+            win.show(animateTarget);
+        }
+    },
 
-        Ext.apply(config, this.getPosition());
+    getLoaderPane : function() {
+        if (!this.loaderPane) {
+            this.loaderPane = Ext.create('Ext.Component', {
+                border: false,
+                flex: 1
+            });
+        }
 
-        this.callParent([config]);
+        return this.loaderPane;
+    },
+
+    getSourcePane : function() {
+        if (!this.sourcePane) {
+            this.sourcePane = Ext.create('Ext.panel.Panel', {
+                border: false,
+                hidden: true,
+                flex: 1,
+                autoScroll: true,
+                cls: 'content',
+                itemSelector: 'div.content-item',
+                store: this.sourcesStore,
+                tpl: new Ext.XTemplate(
+                        '<tpl for=".">',
+                        '<tpl if="category != null">',
+                        '<tpl if="xindex == 1">',
+                        '<div class="content-category">{category:htmlEncode}</div>',
+                        '<tpl elseif="parent[xindex-2].category != category">',
+                        '<div class="content-category content-line">{category:htmlEncode}</div>',
+                        '</tpl>',
+                        '</tpl>',
+                        '<div class="content-item {subjectCount:this.greyMe}">',
+                        '<tpl if="category == \'Assays\'">',
+                        '<div class="content-label">{queryName:htmlEncode}&nbsp;({queryLabel:htmlEncode})</div>',
+                        '<tpl else>',
+                        '<div class="content-label">{queryLabel:htmlEncode}</div>',
+                        '</tpl>',
+                        '<tpl if="subjectCount != -1">',
+                        '<div class="content-count maskit">{subjectCount:this.commaFormat}</div>',
+                        '</tpl>',
+                        '</div>',
+                        '</tpl>',
+                        {
+                            commaFormat : function(v) {
+                                return Ext.util.Format.number(v, '0,000');
+                            },
+                            greyMe : function(v) {
+                                if (v == -1 || v > 0)
+                                    return '';
+                                return 'look-disabled';
+                            },
+                            showLabel : function(queryLabel, longLabel) {
+                                return longLabel && longLabel.length > 0 && (queryLabel != longLabel);
+                            }
+                        }
+                )
+            });
+        }
+
+        return this.sourcePane;
     },
 
     initComponent : function() {
-
         this.items = [
-            this.getForm()
+            this.getHeader(),
+            {html: 'hello'},
+            this.getFooter()
+            // TODO: Create Header,
+            // TODO: Create Form,
+            // TODO: Create Footer,
         ];
-
         this.callParent();
-
-        Ext.EventManager.onWindowResize(this.onResize, this);
-
-        this.resizeTask = new Ext.util.DelayedTask(function() {
-            var pos = this.getPosition(true);
-            this.setPagePosition(pos.x, pos.y);
-        }, this);
-
-        this.on('afterrender', function() {
-
-            // textarea
-            var nav = new Ext.util.KeyNav(this.getTextArea(), {
-                'esc' : function(e) {
-                    this.getForm().getComponent('fbackarea').blur();
-                },
-                'tab' : function(e) {
-                    this.blockBlur = true;
-                    this.submit.focus();
-                    this.blockBlur = false;
-                },
-                scope : this
-            });
-
-            // button
-            var btnNav = new Ext.util.KeyNav(this.submit.getEl(), {
-                'tab' : function(e) {
-                    this.submit.blur();
-                    this.form.getComponent('fbackarea').focus();
-                    this.form.getComponent('fbackarea').blur();
-                },
-                scope : this
-            });
-
-        }, this, {single: true});
     },
 
-    getForm : function() {
-
-        if (this.form) {
-            return this.form;
-        }
-
-        this.submit = Ext.create('Connector.button.RoundedButton', {
-            itemId: 'sbmtfback',
-            ui: 'darkrounded',
-            text  : 'Submit',
-            hidden : true,
-            handler : this.onSubmit,
-            scope : this
-        });
-
-        this.form = Ext.create('Ext.form.Panel', {
-
-            ui : 'custom',
-
-            layout : {
-                align: 'stretch'
-            },
-
-            defaults : {
-                labelAlign : 'top',
-                validateOnBlur : false,
-                validateOnChange : false,
-                width : 225
-            },
-
-            items : [{
-                fieldLabel : 'Feedback',
-                labelSeparator : '',
-                itemId : 'fbackarea',
-                xtype : 'textareafield',
-                name  : 'description',
-                flex  : 1,
-                height: 55,
-                plain      : true,
-                allowBlank : true,
-                emptyText : 'Insights, feature suggestions, bugs...',
-                listeners : {
-                    afterrender : this.onFeedbackRender,
-                    scope : this
+    bindHeader : function(header, data) {
+        if (header && header.getEl() && data) {
+            if (Ext.isFunction(data.action)) {
+                var backActionEl = Ext.DomQuery.select('.back-action', header.getEl().id);
+                if (backActionEl.length > 0) {
+                    Ext.get(backActionEl[0]).on('click', data.action);
                 }
-            },{
-                itemId : 'fbackstate',
-                xtype : 'hidden',
-                name  : 'state'
-            }],
-
-            buttons : [this.submit],
-            buttonAlign : 'left'
-        });
-
-        this.form.getComponent('fbackarea').on('focus', this.onFormFocus, this);
-        this.form.getComponent('fbackarea').on('blur', this.onFormBlur, this);
-
-        return this.form;
-    },
-
-    onFeedbackRender : function(field) {
-        var label = Ext.DomQuery.select('label', field.getEl()['id']);
-        if (label && label.length > 0) {
-            Ext.get(label[0]).applyStyles('float: left;');
-            Ext.DomHelper.insertAfter(label[0], {
-                tag    : 'a',
-                html   : 'Take our survey',
-                cls    : 'survey',
-                target : '_blank',
-                href   : 'https://www.surveymonkey.com/s/CZ56XTF'
-            })
-        }
-    },
-
-    getPosition : function(recalc) {
-
-        if (!recalc && this.posCache) {
-            return this.posCache
-        }
-
-        var win = {
-            x : window.outerWidth,
-            y : window.outerHeight
-        };
-
-        this.posCache = {
-            x : win.x-(290-13),
-            y : win.y-this.yOffset
-        };
-
-        return this.posCache;
-    },
-
-    onFormFocus : function() {
-
-        if (!this.submit.isVisible()) {
-
-            this.getEl().animate({
-                to : {
-                    y : this.getPosition().y-this.yOffset
-                },
-                listeners : {
-                    afteranimate : function() {
-                        this.getForm().getComponent('fbackarea').setHeight(200);
-                        this.submit.show();
-                    },
-                    scope : this
-                },
-                scope : this
-            });
-        }
-    },
-
-    onFormBlur : function() {
-
-        if (this.blockBlur) {
-            return;
-        }
-
-        if (this.submit.isVisible) {
-
-            this.getEl().animate({
-                delay : 150,
-                to : {
-                    y : this.getPosition().y
-                },
-                listeners : {
-                    beforeanimate : function(anim) {
-                        if (!this.buttonpress) {
-                            this.getForm().getComponent('fbackarea').setHeight(55);
-                            this.submit.hide();
-                        }
-                        else {
-                            anim.end();
-                            return false;
-                        }
-                    },
-                    scope : this
-                },
-                scope : this
-            });
-        }
-    },
-
-    onResize : function() {
-        this.onFormBlur();
-        this.resizeTask.delay(500);
-    },
-
-    beforeSubmit : function() {
-        var text = 'Submitting';
-        var val  = '.', count = 1, ellip;
-        var me = this;
-        var textSubmit = function() {
-            ellip = '';
-            for (var x=0; x < count; x++) {
-                ellip += val;
             }
-            for (x=count; x < 3; x++) {
-                ellip += '&nbsp';
-            }
-
-            count >= 3 ? count = 1 : count++;
-            me.submit.setText(text + ellip);
-        };
-
-        this.task = {
-            run : textSubmit,
-            interval : 350
-        };
-
-        this.runner = new Ext.util.TaskRunner();
-        this.runner.start(this.task);
+        }
     },
 
-    onSubmit : function() {
-        this.buttonpress = true;
+    setHeaderData : function(data) {
+        this.headerData = data;
+        this.getHeader().update(data);
+        this.bindHeader(this.getHeader(), data);
+    },
 
-        this.beforeSubmit();
+    getHeader : function() {
+        if (!this.headerPanel) {
+            var initialData = {
+                title: this.headerTitle,
+                showCount: false,
+                border: true
+            };
 
-        var state = this.form.getComponent('fbackstate');
-        if (state && this.getState) {
-            state.setValue(Ext.encode(this.getState().data));
-        }
-
-        if (this.form.getValues().description.length > 0) {
-            LABKEY.Query.insertRows({
-                schemaName : 'CDS',
-                queryName  : 'feedback',
-                rows : [this.getForm().getValues()],
-                success : function(response) {
-                    this.afterSubmit();
-                },
-                failure : function(response) {
-                    console.warn('Failed to post feedback.');
-                    this.afterSubmit();
-                },
-                scope : this
+            var tpl = new Ext.XTemplate(
+                    '<div class="main-title">Provide Feedbeck</div>',
+                    '<div class="sub-title">',
+                    '<span>Give us feedback on what we could improve</span>',
+                    '</div>'
+            );
+            this.headerPanel = Ext.create('Ext.panel.Panel', {
+                cls: 'header',
+                border: true,
+                tpl: tpl,
+                data: initialData,
+                listeners: {
+                    click: function(evt, el) {
+                        console.log('we clicked here!');
+                    },
+                    afterrender: {
+                        fn: function(header) {
+                            this.bindHeader(header, this.headerData ? this.headerData : initialData);
+                        },
+                        scope: this,
+                        single: true
+                    }
+                }
             });
         }
-        else {
-            this.afterSubmit();
-        }
+
+        return this.headerPanel;
     },
 
-    afterSubmit : function() {
-        if (this.runner && this.task) {
-            this.runner.stop(this.task);
+    getFooter : function() {
+        XX = this;
+        if (!this.footerPanel) {
+            this.footerPanel = Ext.create('Ext.panel.Panel', {
+                bodyCls: 'footer',
+                border: true,
+                layout: {
+                    type: 'hbox',
+                    pack: 'end'
+                },
+                items: [{
+                    itemId: 'cancel-link',
+                    xtype: 'button',
+                    ui: 'rounded-inverted-accent-text',
+                    hidden: false,
+                    text: 'Cancel',
+                    listeners: {
+                        click: function(evt, el) {
+                            XX.hide();
+                        },
+                        element: 'el',
+                        scope: this
+                    },
+                    handler: function() {
+                        this.fireEvent('cancel');
+                    },
+                    scope: this
+                },{
+                    itemId: 'done-button',
+                    xtype: 'button',
+                    text: 'Done',
+                    listeners: {
+                        click: function(evt, el) {
+                            XX.hide();
+                        },
+                        element: 'el',
+                        scope: this
+                    },
+                    scope: this
+                }]
+            });
         }
 
-        this.submit.setText('Thank You!');
-        this.getForm().getComponent('fbackarea').reset();
-        this.setEmptyText('Thank you for your feedback!');
-
-        var wrapupTask = new Ext.util.DelayedTask(function() {
-            this.buttonpress = false;
-            this.onFormBlur();
-            this.submit.setText('Submit');
-        }, this);
-        wrapupTask.delay(1250);
-    },
-
-    getTextArea : function() {
-        return Ext.DomQuery.select('textarea[name=description]', this.getForm().items.items[0].getEl().id)[0];
-    },
-
-    setEmptyText : function(text) {
-        this.getForm().items.items[0].emptyText = text;
-        if (this.rendered) {
-            this.getTextArea().placeholder = text;
-        }
+        return this.footerPanel;
     }
 });
