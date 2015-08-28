f6e9593b65983f230957d90049ef478a404dfc60
Nick Arnold <nicka@labkey.com>
2015-08-28 14:13:26 -0700

    Re-enable single use portion of ds_visittagmap - switch to using study_arm_visit_label

--- resources/queries/cds/ds_visittagmap.sql
+++ resources/queries/cds/ds_visittagmap.sql
@@ -12,17 +12,17 @@ JOIN (SELECT * FROM cds.studygroup) AS studygroup ON (studygroup.container.name 
 JOIN (SELECT * FROM study.visit) AS visit ON (visit.Folder.Name = arm_visit.prot AND visit.sequencenummin = CAST(arm_visit.study_day AS DOUBLE))
 WHERE arm_visit.study_arm_visit_type IS NOT NULL
 
--- UNION
---
--- SELECT
--- DISTINCT arm_visit.study_arm_visit_label AS visit_tag,
--- visit.rowid AS visit_row_id,
--- studygroup.row_id AS study_group_id,
--- arm_visit.prot,
--- arm_visit.study_arm_visit_detail_label AS visit_tag_label,
--- TRUE AS single_use,
--- FALSE AS is_vaccination,
--- FROM cds.import_studypartgrouparmvisit AS arm_visit
--- JOIN (SELECT * FROM cds.studygroup) AS studygroup ON (studygroup.container.name = arm_visit.prot AND studygroup.group_name = arm_visit.study_group)
--- JOIN (SELECT * FROM study.visit) AS visit ON (visit.Folder.Name = arm_visit.prot AND visit.sequencenummin = CAST(arm_visit.study_day AS DOUBLE))
--- WHERE arm_visit.study_arm_visit_label IS NOT NULL
+UNION
+
+SELECT
+DISTINCT arm_visit.study_arm_visit_label AS visit_tag,
+visit.rowid AS visit_row_id,
+studygroup.row_id AS study_group_id,
+arm_visit.prot,
+arm_visit.study_arm_visit_label AS visit_tag_label,
+TRUE AS single_use,
+FALSE AS is_vaccination,
+FROM cds.import_studypartgrouparmvisit AS arm_visit
+JOIN (SELECT * FROM cds.studygroup) AS studygroup ON (studygroup.container.name = arm_visit.prot AND studygroup.group_name = arm_visit.study_group)
+JOIN (SELECT * FROM study.visit) AS visit ON (visit.Folder.Name = arm_visit.prot AND visit.sequencenummin = CAST(arm_visit.study_day AS DOUBLE))
+WHERE arm_visit.study_arm_visit_label IS NOT NULL
