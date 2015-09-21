a47cc1e197aad2baa2c5ab17f01b881869d9c8d5
Nick Arnold <nicka@labkey.com>
2015-09-21 15:09:41 -0700

    Plot: Update brushing and highlighting color

--- webapp/Connector/src/view/Chart.js
+++ webapp/Connector/src/view/Chart.js
@@ -916,6 +916,8 @@ Ext.define('Connector.view.Chart', {
             var onBrush = this.showPointsAsBin ? ChartUtils.brushBins : ChartUtils.brushPoints;
 
             plotConfig.brushing = {
+                fillColor: ChartUtils.colors.SELECTEDBG,
+                strokeColor: ChartUtils.colors.SELECTED,
                 dimension: !properties.xaxis.isDimension && properties.xaxis.isContinuous ? 'both' : 'y',
                 brushstart : Ext.bind(function() {
                     this.clearHighlightLabels(layerScope.plot);
