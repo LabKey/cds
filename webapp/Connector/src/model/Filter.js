dc6e126b4457313be359d8a0eee30a505ad5b138
Nick Arnold <nicka@labkey.com>
2015-10-06 15:01:00 -0700

    Filtering: Generate data filters per type - fulfilled upon filter construction - only covers simple uses cases at the moment

--- webapp/Connector/src/model/Filter.js
+++ webapp/Connector/src/model/Filter.js
@@ -6,6 +6,10 @@
 Ext.define('Connector.model.Filter', {
     extend: 'LABKEY.app.model.Filter',
 
+    fields: [
+        {name : 'dataFilter', defaultValue: {}}
+    ],
+
     statics: {
         loaded: false,
         subjectMap: {},
@@ -58,5 +62,132 @@ Ext.define('Connector.model.Filter', {
 
             return values;
         }
+    },
+
+    constructor : function(config) {
+        this.callParent([config]);
+
+        Connector.getQueryService().onQueryReady(function() {
+            this._generateDataFilters();
+        }, this);
+    },
+
+    modify : function(datas) {
+
+    },
+
+    _dataFilterHelper : function(filterMap, alias, filter) {
+        if (!Ext.isArray(filterMap[alias])) {
+            filterMap[alias] = [];
+        }
+        filterMap[alias].push(filter);
+    },
+
+    _generateDataFilters : function() {
+
+        var dataFilterMap = {};
+
+        if (this.isPlot() && this.isGrid()) {
+            // plot selection filter
+
+            /**
+             * "Plot selection filters, including axis filters, are global data filters on the dragged measure
+             * (each axis considered separately), unless both axes are the same source then plot selection filters,
+             * including axis filters, are applied as a compound global data filter."
+             */
+
+            // TODO: Compare the axes to determine if they meet the "unless" condition stated above
+
+            Ext.each(this.get('gridFilter'), function(gridFilter) {
+                if (gridFilter) {
+                    this._dataFilterHelper(dataFilterMap, gridFilter.getColumnName(), gridFilter);
+                }
+            }, this);
+
+            Ext.each(this.get('plotMeasures'), function(plotMeasure) {
+                if (plotMeasure && plotMeasure.measure) {
+
+                    // axis filters -> data filters
+                    var measure = plotMeasure.measure;
+                    if (measure.options && measure.options.dimensions) {
+                        Ext.iterate(measure.options.dimensions, function(columnName, values) {
+                            if (Ext.isArray(values) && !Ext.isEmpty(values)) {
+                                // TODO: Switch axis filters to using alias rather than column name.
+                                // TODO: Once done, the dependency on QueryService can be removed
+                                var genFilter = this._generateFilter(measure, columnName, values);
+                                if (genFilter) {
+                                    this._dataFilterHelper(dataFilterMap, genFilter.getColumnName(), genFilter);
+                                }
+                            }
+                        }, this);
+                    }
+                }
+            }, this);
+        }
+        else if (this.isPlot()) {
+            // in the plot filter
+
+            // "In the Plot filters, including the axis filters, are applied as a data filter globally."
+            Ext.each(this.get('plotMeasures'), function(plotMeasure) {
+                if (plotMeasure && plotMeasure.measure) {
+
+                    // axis filters -> data filters
+                    var measure = plotMeasure.measure;
+                    if (measure.options && measure.options.dimensions) {
+                        Ext.iterate(measure.options.dimensions, function(columnName, values) {
+                            if (Ext.isArray(values) && !Ext.isEmpty(values)) {
+                                // TODO: Switch axis filters to using alias rather than column name.
+                                // TODO: Once done, the dependency on QueryService can be removed
+                                var genFilter = this._generateFilter(measure, columnName, values);
+                                if (genFilter) {
+                                    this._dataFilterHelper(dataFilterMap, genFilter.getColumnName(), genFilter);
+                                }
+                            }
+                        }, this);
+                    }
+
+                    // TODO: Deal with situational filters (e.g. log plots use "measure > 0" filter)
+                }
+            }, this);
+        }
+        else if (this.isGrid()) {
+            // grid filter
+            Ext.each(this.get('gridFilter'), function(gridFilter) {
+                if (gridFilter) {
+                    this._dataFilterHelper(dataFilterMap, gridFilter.getColumnName(), gridFilter);
+                }
+            }, this);
+        }
+        else {
+            // olap filter -- nothing to do
+        }
+
+        this.set('dataFilter', dataFilterMap);
+    },
+
+    /**
+     * Attempts to generate a filter from the specified
+     * @param measure
+     * @param columnName
+     * @param values
+     * @private
+     */
+    _generateFilter : function(measure, columnName, values) {
+        var alias = [measure.schemaName, measure.queryName, columnName].join('_'),
+            queryMeasure = Connector.getQueryService().getMeasure(alias),
+            filter;
+
+        if (!queryMeasure) {
+            throw 'Unable to resolve filter alias: ' + alias;
+        }
+
+        if (values.length > 1) {
+            filter = LABKEY.Filter.create(queryMeasure.alias, values, LABKEY.Filter.Types.EQUALS_ONE_OF);
+        }
+        else {
+            filter = LABKEY.Filter.create(queryMeasure.alias, values[0]);
+        }
+
+        return filter;
     }
 });
