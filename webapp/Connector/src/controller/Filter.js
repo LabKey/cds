3989e6ea00e49870a5f85ebf8424134d36da5bd5
Nick Arnold <nicka@labkey.com>
2015-10-26 08:16:11 -0700

    Filter service: getSubjectVisits() - allows for runtime lookup of subject visits within any range / any alignment

--- webapp/Connector/src/controller/Filter.js
+++ webapp/Connector/src/controller/Filter.js
@@ -23,6 +23,23 @@ Ext.define('Connector.controller.Filter', {
                 this.updateSubjects(mdx, state.getFilters(), this.onFilterChange, this);
             }, this);
         }, this);
+
+        this._tagCache = [];
+        this._visitCache = [];
+
+        Ext.create('Connector.store.VisitTag', {
+            singleUseOnly: true,
+            autoLoad: true,
+            listeners: {
+                load: {
+                    fn: this.onVisitTagLoad,
+                    scope: this,
+                    single: true
+                }
+            }
+        });
+
+        this._loadSubjectVisits();
     },
 
     updateSubjects : function(mdx, filters, callback, scope) {
@@ -115,5 +132,153 @@ Ext.define('Connector.controller.Filter', {
             subjects: undefined,
             results: currentResults
         };
+    },
+
+    getSubjectVisits : function(alignmentAlias, min, max, callback, scope)
+    {
+        if (this._visitsLoaded)
+        {
+            this._querySubjectVisits(alignmentAlias, min, max, callback, scope);
+        }
+        else
+        {
+            this._visitCache.push({
+                alias: alignmentAlias,
+                min: min,
+                max: max,
+                callback: callback,
+                scope: scope
+            });
+        }
+    },
+
+    _querySubjectVisits : function(alias, min, max, callback, scope)
+    {
+        if (!this._visitsLoaded)
+        {
+            throw '_querySubjectVisits() cannot be called before the visit cache is loaded. Use getSubjectVisits()';
+        }
+
+        var subjectVisits = [],
+            realMin = Ext.isNumber(min) ? min : -10000000,
+            realMax = Ext.isNumber(max) ? max : 10000000,
+            value, i;
+
+        if (!Ext.isEmpty(this._visitData))
+        {
+            // spot check alias
+            if (!alias in this._visitData[0])
+            {
+                console.warn('"' + alias + '" may be an invalid time alias.');
+            }
+
+            for (i=0; i < this._visitData.length; i++)
+            {
+                value = this._visitData[i][alias];
+
+                if (value >= realMin && value <= realMax)
+                {
+                    subjectVisits.push(this._visitData[i][QueryUtils.SUBJECT_SEQNUM_ALIAS]);
+                }
+            }
+        }
+
+        callback.call(scope, subjectVisits);
+    },
+
+    _loadSubjectVisits : function()
+    {
+        Connector.getQueryService().onQueryReady(function(queryService)
+        {
+            var measures = [],
+                timeMeasures = [];
+
+            Ext.iterate(queryService.getTimeAliases(), function(alias)
+            {
+                timeMeasures.push(queryService.getMeasure(alias));
+            });
+
+            this.getVisitTags(function(tags)
+            {
+                Ext.each(timeMeasures, function(measure)
+                {
+                    // Day/Week/Month 0
+                    measures.push({
+                        measure: measure,
+                        dateOptions: {
+                            interval: measure.alias,
+                            zeroDayVisitTag: null
+                        }
+                    });
+
+                    Ext.iterate(tags, function(tag)
+                    {
+                        measures.push({
+                            measure: measure,
+                            dateOptions: {
+                                interval: measure.alias,
+                                zeroDayVisitTag: tag
+                            }
+                        });
+                    });
+                }, this);
+
+                queryService.getData(measures, function(metadata)
+                {
+                    LABKEY.Query.selectRows({
+                        schemaName: metadata.schemaName,
+                        queryName: metadata.queryName,
+                        filterArray: [
+                            LABKEY.Filter.create(QueryUtils.SEQUENCENUM_ALIAS, 0, LABKEY.Filter.Types.GREATER_THAN_OR_EQUAL)
+                        ],
+                        success: function(data)
+                        {
+                            this._visitData = data.rows;
+                            this._visitsLoaded = true;
+
+                            Ext.each(this._visitCache, function(cached)
+                            {
+                                this._querySubjectVisits(cached.alias, cached.min, cached.max, cached.callback, cached.scope);
+                            }, this);
+                        },
+                        scope: this
+                    });
+                }, undefined, this);
+            }, this);
+        }, this);
+    },
+
+    getVisitTags : function(callback, scope)
+    {
+        if (this._tagsLoaded)
+        {
+            callback.call(scope, Ext.clone(this._tags));
+        }
+        else
+        {
+            this._tagCache.push({
+                fn: callback,
+                scope: scope
+            })
+        }
+    },
+
+    onVisitTagLoad : function(store)
+    {
+        this._tags = {};
+
+        Ext.each(store.getRange(), function(tag)
+        {
+            this._tags[tag.get('Name')] = 1;
+        }, this);
+
+        this._tagsLoaded = true;
+
+        Ext.each(this._tagCache, function(cache)
+        {
+            cache.fn.call(cache.scope, Ext.clone(this._tags));
+        }, this);
+
+        this._tagCache = [];
     }
 });
