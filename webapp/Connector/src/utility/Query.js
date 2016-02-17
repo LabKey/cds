2a758e7bfb273ffdafc16f697070d6eee17bf473
labkey-eyounske <eyounske@labkey.com>
2016-02-16 20:06:16 -0800

    Update copyrights to 2016 in applicable files

--- webapp/Connector/src/utility/Query.js
+++ webapp/Connector/src/utility/Query.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015-2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 // This is a singleton for cds getData query utilities
 Ext.define('Connector.utility.Query', {
 
