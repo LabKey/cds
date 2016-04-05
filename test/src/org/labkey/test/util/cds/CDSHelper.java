3fac4e925cce67d8c74261ad4a147524fcee09dd
XingY <xyang@labkey.com>
2016-04-05 16:23:12 -0700

    Issue 463: Discrete time - recommended vs additional

--- test/src/org/labkey/test/util/cds/CDSHelper.java
+++ test/src/org/labkey/test/util/cds/CDSHelper.java
@@ -372,12 +372,12 @@ public class CDSHelper
     public static final String NAB_VIRUS_TYPE = "Virus type";
 
     public static final String TIME_POINTS = "Time points";
-    public static final String TIME_POINTS_DAYS = "Study days";
-    public static final String TIME_POINTS_WEEKS = "Study weeks";
-    public static final String TIME_POINTS_MONTHS = "Study months";
-    public static final String TIME_POINTS_DISCRETE_DAYS = "Study days (discrete)";
-    public static final String TIME_POINTS_DISCRETE_WEEKS = "Study weeks (discrete)";
-    public static final String TIME_POINTS_DISCRETE_MONTHS = "Study months (discrete)";
+    public static final String TIME_POINTS_DAYS = "Study days (timeline)";
+    public static final String TIME_POINTS_WEEKS = "Study weeks (timeline)";
+    public static final String TIME_POINTS_MONTHS = "Study months (timeline)";
+    public static final String TIME_POINTS_DISCRETE_DAYS = "Study days (box plots)";
+    public static final String TIME_POINTS_DISCRETE_WEEKS = "Study weeks (box plots)";
+    public static final String TIME_POINTS_DISCRETE_MONTHS = "Study months (box plots)";
 
     // These are values used in the data grid.
     public static final String GRID_TITLE_BAMA = TITLE_BAMA;
