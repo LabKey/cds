c3025fc9d17a6c86f4ce86bb5b8838c7f3931d00
Joe Christianson <joec@labkey.com>
2016-06-23 18:49:17 -0700

    Bug Fixing -Added select distinct to ds_studyproduct.sql -Added missing columns to cds.xml (should address SchemaXMLTestCase) failures -Rolled back changes to test data. -Added rows to studyProducts.txt to exercise feature, without breaking tests.

--- resources/queries/cds/ds_studyproduct.sql
+++ resources/queries/cds/ds_studyproduct.sql
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-SELECT
+SELECT DISTINCT
    md.prot AS study_name,
    md.prot,
    md.product_id,
