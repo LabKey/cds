9ac0dd38e996c9c3efb43ec5ab62e9244c59ad69
Joe <joec@labkey.com>
2016-08-11 13:18:51 -0700

    Spec Issue 27196: Add columns/horizontal scrolling  - Fix filtering window to work with locked columns

--- webapp/Connector/src/app/view/LearnSummary.js
+++ webapp/Connector/src/app/view/LearnSummary.js
@@ -13,36 +13,39 @@ Ext.define('Connector.app.view.LearnSummary', {
         beforerender: function (grid)
         {
             var learnView = this.learnView;
-            var header = grid.down('headercontainer'), dim = grid.dimension ? grid.dimension.name : undefined;
-            header.on('headertriggerclick', function onTriggerClick(headerCt, column)
-            {
-                var field = column.filterConfig.filterField;
-                Ext.create('Connector.window.LearnFacet', {
-                    dim: dim,
-                    filterConfig: column.filterConfig,
-                    col: column,
-                    columnMetadata: {caption : column.filterConfig.title},
-                    learnStore: this.store,
-                    dataView: this,
-                    filterValues: learnView.columnFilters[field] ? learnView.columnFilters[field] : [],
-                    listeners: {
-                        filter: function (filterValues)
-                        {
-                            this.learnView.getHeader().fireEvent('updateLearnFilter', field, filterValues);
-                        },
-                        clearfilter: function ()
-                        {
-                            this.learnView.getHeader().fireEvent('updateLearnFilter', field, []);
+            var headers = grid.query('headercontainer'),
+                dim = grid.dimension ? grid.dimension.name : undefined;
+            Ext.each(headers, function(header) {
+                header.on('headertriggerclick', function onTriggerClick(headerCt, column)
+                {
+                    var field = column.filterConfig.filterField;
+                    Ext.create('Connector.window.LearnFacet', {
+                        dim: dim,
+                        filterConfig: column.filterConfig,
+                        col: column,
+                        columnMetadata: {caption : column.filterConfig.title},
+                        learnStore: this.store,
+                        dataView: this,
+                        filterValues: learnView.columnFilters[field] ? learnView.columnFilters[field] : [],
+                        listeners: {
+                            filter: function (filterValues)
+                            {
+                                this.learnView.getHeader().fireEvent('updateLearnFilter', field, filterValues);
+                            },
+                            clearfilter: function ()
+                            {
+                                this.learnView.getHeader().fireEvent('updateLearnFilter', field, []);
+                            },
+                            scope: this
                         },
                         scope: this
-                    },
-                    scope: this
-                });
-                return false;
-            }, this);
-            header.on('sortchange', function (headerCt, column, direction)
-            {
-                this.learnView.getHeader().fireEvent('updateLearnSort', column.dataIndex, direction);
+                    });
+                    return false;
+                }, this);
+                header.on('sortchange', function (headerCt, column, direction)
+                {
+                    this.learnView.getHeader().fireEvent('updateLearnSort', column.dataIndex, direction);
+                }, this);
             }, this);
         },
 
