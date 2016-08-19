abfdf23fe76190d76c0c3dc20d7d390b5ef350ff
XingY <xyang@labkey.com>
2016-08-18 18:13:42 -0700

    Issue 27520: Add "i" icon for Learn About page link to hovers for Products from Info Pane

--- webapp/Connector/cube.js
+++ webapp/Connector/cube.js
@@ -352,7 +352,8 @@ Ext4.define('Connector.cube.Configuration', {
                         activeCount: true,
                         countPriority: 30,
                         countSingular: 'Product',
-                        countPlural: 'Products'
+                        countPlural: 'Products',
+                        supportsLearn: true
                     }]
                 },{
                     uniqueName: '[Study Product.Product Type]',
