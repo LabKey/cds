43dc176c3ca7ea34520e1f6343656a875a41d906
Joe <joec@labkey.com>
2016-09-19 10:10:59 -0700

    Spec Issue: 27480  - Fixed schema and ETL to work with sample data from client  - Added blank files to match paths given by client.

--- resources/queries/cds/ds_studypublication.sql
+++ resources/queries/cds/ds_studypublication.sql
@@ -1,2 +1,5 @@
-SELECT *
+SELECT
+  prot AS prot,
+  publication_id AS publication_id,
+  study_publication_order AS publication_order,
 FROM cds.import_StudyPublication
--- resources/schemas/dbscripts/postgresql/cds-16.25-16.26.sql
+++ resources/schemas/dbscripts/postgresql/cds-16.25-16.26.sql
@@ -1,42 +1,50 @@
 CREATE TABLE cds.import_Publication (
   publication_id VARCHAR(250) NOT NULL,
-  label VARCHAR(250),
-  filename VARCHAR(255),
-  publication_type VARCHAR(250),
-  is_external BOOLEAN,
-  sort_index INTEGER,
+  publication_title TEXT,
+  publication_author_all TEXT,
+  publication_journal_short VARCHAR(250),
+  publication_date VARCHAR(250),
+  publication_volume VARCHAR(250),
+  publication_issue VARCHAR(250),
+  publication_location VARCHAR(250),
+  publication_pmid VARCHAR(250),
+  publication_link VARCHAR(250),
 
-  CONSTRAINT PK_import_Publication PRIMARY KEY (publication_id)
+  CONSTRAINT pk_import_Publication PRIMARY KEY (publication_id)
 );
 
 CREATE TABLE cds.import_StudyPublication (
   prot VARCHAR(250) NOT NULL,
   publication_id VARCHAR(250) NOT NULL,
+  study_publication_order VARCHAR(250),
 
   CONSTRAINT pk_import_StudyPublication PRIMARY KEY (prot, publication_id)
 );
 
 CREATE TABLE cds.Publication (
-  publication_id VARCHAR(250) NOT NULL,
+  id VARCHAR(250) NOT NULL,
   container entityid NOT NULL,
-  label VARCHAR(250),
-  filename VARCHAR(255),
-  publication_type VARCHAR(250),
-  is_external BOOLEAN,
-  sort_index INTEGER,
+  title TEXT,
+  author_all TEXT,
+  journal_short VARCHAR(250),
+  date VARCHAR(250),
+  volume VARCHAR(250),
+  issue VARCHAR(250),
+  location VARCHAR(250),
+  pmid VARCHAR(250),
+  link VARCHAR(250),
 
-  CONSTRAINT PK_Publication PRIMARY KEY (publication_id)
+  CONSTRAINT PK_Publication PRIMARY KEY (id)
 );
 
 CREATE TABLE cds.StudyPublication (
   prot VARCHAR(250) NOT NULL,
   publication_id VARCHAR(250) NOT NULL,
   container entityid NOT NULL,
+  publication_order VARCHAR(250),
 
   CONSTRAINT pk_StudyPublication PRIMARY KEY (prot, publication_id)
 );
 
-ALTER TABLE cds.import_document ADD COLUMN is_external BOOLEAN;
-ALTER TABLE cds.document ADD COLUMN is_external BOOLEAN;
-ALTER TABLE cds.import_document ADD COLUMN sort_index INTEGER;
-ALTER TABLE cds.document ADD COLUMN sort_index INTEGER;
+ALTER TABLE cds.import_studydocument ADD COLUMN study_document_order INTEGER;
+ALTER TABLE cds.StudyDocument ADD COLUMN document_order INTEGER;
