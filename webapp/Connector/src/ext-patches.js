49579ec85074fa8cf3cc5c813d4c411c63c1d70e
XingY <xyang@labkey.com>
2017-03-21 15:52:40 -0700

    Issue 28703: Inconsistent highlighting on Learn About summary pages

--- webapp/Connector/src/ext-patches.js
+++ webapp/Connector/src/ext-patches.js
@@ -6,38 +6,70 @@
  * forum post: https://www.sencha.com/forum/showthread.php?296278. This function caused the resulting performance hit.
  * Removing it causes no observable issues, but to be safe only apply this patch to CDS.
  */
-Ext.override(Ext.grid.locking.Lockable, {
-    // syncRow with locked grid causes performance issue
-    // resort to css to ensure same row height for locked row and normal row instead
-    syncRowHeight: false,
-    onLockedViewScroll: function ()
-    {
-        if (!this.lockedViewScrollTask)
+if (Ext.isIE)
+{
+    Ext.override(Ext.grid.locking.Lockable, {
+        // syncRow with locked grid causes performance issue
+        // resort to css to ensure same row height for locked row and normal row instead
+        syncRowHeight: false,
+        onLockedViewScroll: function ()
         {
-            this.lockedViewScrollTask = new Ext.util.DelayedTask(this.onLockedViewScrollTask, this);
-        }
-        this.lockedViewScrollTask.delay(100, undefined, this);
-    },
-    onLockedViewScrollTask: function() {
-        var me = this,
-                lockedView = me.lockedGrid.getView(),
-                normalView = me.normalGrid.getView(),
-                normalDom = normalView.el.dom,
-                lockedDom = lockedView.el.dom,
-                normalTable,
-                lockedTable;
+            if (!this.lockedViewScrollTask)
+            {
+                this.lockedViewScrollTask = new Ext.util.DelayedTask(this.onLockedViewScrollTask, this);
+            }
+            this.lockedViewScrollTask.delay(100, undefined, this);
+        },
+        onLockedViewScrollTask: function() {
+            var me = this,
+                    lockedView = me.lockedGrid.getView(),
+                    normalView = me.normalGrid.getView(),
+                    normalDom = normalView.el.dom,
+                    lockedDom = lockedView.el.dom,
+                    normalTable,
+                    lockedTable;
 
-        // See onNormalViewScroll
-        if (normalDom.scrollTop !== lockedDom.scrollTop) {
-            normalDom.scrollTop = lockedDom.scrollTop;
+            // See onNormalViewScroll
+            if (normalDom.scrollTop !== lockedDom.scrollTop) {
+                normalDom.scrollTop = lockedDom.scrollTop;
 
-            // For buffered views, the absolute position is important as well as scrollTop
-            if (me.store.buffered) {
-                lockedTable = lockedView.el.child('table', true);
-                normalTable = normalView.el.child('table', true);
-                normalTable.style.position = 'absolute';
-                normalTable.style.top = lockedTable.style.top;
+                // For buffered views, the absolute position is important as well as scrollTop
+                if (me.store.buffered) {
+                    lockedTable = lockedView.el.child('table', true);
+                    normalTable = normalView.el.child('table', true);
+                    normalTable.style.position = 'absolute';
+                    normalTable.style.top = lockedTable.style.top;
+                }
             }
         }
+    });
+}
+
+Ext.override(Ext.grid.View, {
+    setHighlightedItem: function(item) {
+        if (!this.setHighlightedItemTask)
+        {
+            this.setHighlightedItemTask = new Ext.util.DelayedTask(this.onSetHighlightedItemTask, this);
+        }
+        if (!item)
+            this.setHighlightedItemTask.delay(200, undefined, this, [item]);
+        else
+        {
+            this.setHighlightedItemTask.delay(200, undefined, this, [item]);
+        }
+    },
+    onSetHighlightedItemTask: function(item)
+    {
+        var me = this,
+                highlighted = me.highlightedItem;
+
+        if (highlighted && me.el.isAncestor(highlighted) && me.isRowStyleFirst(highlighted)) {
+            me.getRowStyleTableEl(highlighted).removeCls(me.tableOverFirstCls);
+        }
+
+        if (item && me.isRowStyleFirst(item)) {
+            me.getRowStyleTableEl(item).addCls(me.tableOverFirstCls);
+        }
+        me.superclass.setHighlightedItem.apply(this, arguments);
     }
 });
