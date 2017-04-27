716962056523558332baee8fc6ef7a9d303b52e7
XingY <xyang@labkey.com>
2017-04-27 16:32:50 -0700

    Revert change to applying patch only for IE as syncing is required for trackpad scrolling

--- webapp/Connector/src/ext-patches.js
+++ webapp/Connector/src/ext-patches.js
@@ -6,44 +6,41 @@
  * forum post: https://www.sencha.com/forum/showthread.php?296278. This function caused the resulting performance hit.
  * Removing it causes no observable issues, but to be safe only apply this patch to CDS.
  */
-if (Ext.isIE || Ext.isWindows)
-{
-    Ext.override(Ext.grid.locking.Lockable, {
-        // syncRow with locked grid causes performance issue
-        // resort to css to ensure same row height for locked row and normal row instead
-        syncRowHeight: false,
-        onLockedViewScroll: function ()
-        {
-            if (!this.lockedViewScrollTask)
-            {
-                this.lockedViewScrollTask = new Ext.util.DelayedTask(this.onLockedViewScrollTask, this);
-            }
-            this.lockedViewScrollTask.delay(100, undefined, this);
-        },
-        onLockedViewScrollTask: function() {
-            var me = this,
-                    lockedView = me.lockedGrid.getView(),
-                    normalView = me.normalGrid.getView(),
-                    normalDom = normalView.el.dom,
-                    lockedDom = lockedView.el.dom,
-                    normalTable,
-                    lockedTable;
 
-            // See onNormalViewScroll
-            if (normalDom.scrollTop !== lockedDom.scrollTop) {
-                normalDom.scrollTop = lockedDom.scrollTop;
+Ext.override(Ext.grid.locking.Lockable, {
+    // syncRow with locked grid causes performance issue
+    // resort to css to ensure same row height for locked row and normal row instead
+    syncRowHeight: false,
+    onLockedViewScroll: function () {
+        if (!this.lockedViewScrollTask) {
+            this.lockedViewScrollTask = new Ext.util.DelayedTask(this.onLockedViewScrollTask, this);
+        }
+        this.lockedViewScrollTask.delay(100, undefined, this);
+    },
+    onLockedViewScrollTask: function () {
+        var me = this,
+                lockedView = me.lockedGrid.getView(),
+                normalView = me.normalGrid.getView(),
+                normalDom = normalView.el.dom,
+                lockedDom = lockedView.el.dom,
+                normalTable,
+                lockedTable;
 
-                // For buffered views, the absolute position is important as well as scrollTop
-                if (me.store.buffered) {
-                    lockedTable = lockedView.el.child('table', true);
-                    normalTable = normalView.el.child('table', true);
-                    normalTable.style.position = 'absolute';
-                    normalTable.style.top = lockedTable.style.top;
-                }
+        // See onNormalViewScroll
+        if (normalDom.scrollTop !== lockedDom.scrollTop) {
+            normalDom.scrollTop = lockedDom.scrollTop;
+
+            // For buffered views, the absolute position is important as well as scrollTop
+            if (me.store.buffered) {
+                lockedTable = lockedView.el.child('table', true);
+                normalTable = normalView.el.child('table', true);
+                normalTable.style.position = 'absolute';
+                normalTable.style.top = lockedTable.style.top;
             }
         }
-    });
-}
+    }
+});
+
 
 Ext.override(Ext.grid.View, {
     setHighlightedItem: function(item) {
