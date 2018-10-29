3676d50823cfb14e9b23e0f2db7a5c5715a623a8
labkey-tchad <tchad@labkey.com>
2018-10-29 12:45:31 -0700

    Use java.util.Collections to create unmodifiable collections

--- test/src/org/labkey/test/pages/cds/MAbDataGrid.java
+++ test/src/org/labkey/test/pages/cds/MAbDataGrid.java
@@ -11,10 +11,10 @@ import org.labkey.test.util.cds.CDSHelper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -423,7 +423,7 @@ public class MAbDataGrid extends WebDriverComponent<MAbDataGrid.ElementCache>
         protected List<WebElement> getColumnHeaders()
         {
             if (columnHeaders == null)
-                columnHeaders = ImmutableList.copyOf(Locators.columnHeader.findElements(mabGrid));
+                columnHeaders = Collections.unmodifiableList(Locators.columnHeader.findElements(mabGrid));
             return columnHeaders;
         }
     }
