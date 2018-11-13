1523a7811e75366e4949eb4eaa5f185370cba35b
labkey-eyounske <eyounske@labkey.com>
2018-11-12 16:50:03 -0800

    Copyright and license updates   -- Update for November 2018   -- Now scanning .tf and .tfvars files (Terraform)   -- Deliberately leaving out SNPRC module (moving to Git very soon)

--- webapp/Connector/src/app/view/module/StudyMabs.js
+++ webapp/Connector/src/app/view/module/StudyMabs.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext.define('Connector.view.module.StudyMabs', {
 
     xtype : 'app.module.studymabs',
--- webapp/Connector/src/view/HeaderDataView.js
+++ webapp/Connector/src/view/HeaderDataView.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017-2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 Ext.define('Connector.view.HeaderDataView', {
 
     extend: 'Ext.view.View',
