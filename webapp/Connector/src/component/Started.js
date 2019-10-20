03483d5d7bb1de3bfe8bdd141189aeb5bc9d3cb9
Binal <binalp@labkey.com>
2019-10-20 13:39:39 -0700

    Issue 38708: Fix home page layout issues

--- webapp/Connector/src/component/Started.js
+++ webapp/Connector/src/component/Started.js
@@ -15,84 +15,99 @@ Ext.define('Connector.component.Started', {
 
     tpl: new Ext.XTemplate(
             '<div class="expanded-intro" id="expanded-intro-div"',
-                '<tpl if="showIntro==false">',
-                    ' style="display: none !important;"',
-                '</tpl>',
+            '<tpl if="showIntro==false">',
+                ' style="display: none !important;"',
+            '</tpl>',
             '>',
-                '<h1 class="started-section-title bottom-spacer">Quick Links</h1>',
-                '<a id="hidelink" href="#" onclick="return false;" class="started-dismiss">Hide &nbsp; <span style="font-size: 9px">&#x2715;</span></a>',
-                    '<tpl if="videoURL">',
-                        '<div class="get-started get-started-multiRow">',
-                        '<table class="tile-group" id="try-it-out-links">',
-                        '<tr>',
-                            '<td class="tile tile-image"><div class="home_video backgroundimage"></div></td>',
-                            '<td class="tile tile-image"><div class="home_bar backgroundimage"></div></td>',
-                            '<td class="tile tile-image"><div class="home_try_it_out backgroundimage"></div></td>',
-                            '<td class="tile tile-image"><div class="home_integrated_data backgroundimage"></div></td>',
-                        '</tr>',
-                        '<tr>',
-                            '<td class="tile-detail" style="padding-left:5%;"><h3 class="tile-title">Watch the <a style="cursor:pointer" class="tile" id="home-video">Get Started</a> video</h3>',
-                                '<div><table>',
-                                    '<tr><td>See the most powerful ways to explore the DataSpace.</td></tr>',
-                                '</table></div>',
-                            '</td>',
-                            '<td class="tile-detail" style="padding-left:10%;">',
-                                '<div id="tours-wiki"><table>',
-                                    '<tr><td>{[this.getTakeATourWiki()]}</td></tr>',
-                                '</table></div>',
-                            '</td>',
-                            '<td class="tile-detail" style="padding-left:10%"><h3 class="tile-title">Try it out</h3>',
-                                '<div><table>',
-                                    '<tr><td class="tile-detail-static"><a id="learn-about-link" href=\'#learn/learn/Study/\'">Learn about</a> studies, products, assays, antibodies, ad publications</td></td></tr>',
-                                    '<tr><td class="tile-detail-static"><a id="find-subjects-link" href=\'#summary\'">Find subjects</a> with common characteristics</td></td></tr>',
-                                    '<tr><td class="tile-detail-static"><a id="plot-link" href=\'#chart\'">Plot</a> assay results across studies and years of research</td></td></tr>',
-                                    '<tr><td class="tile-detail-static">Compare <a id="monoclonal-antibodies-link" href=\'#mabgrid\'">Monoclonal antibodies</a> and their neutralization curves</td></td></tr>',
-                                '</table></div>',
-                            '</td>',
-                            '<td class="tile-detail" style="padding-left:10%">',
-                                '<div id="whatyouneedtoknow-wiki"><table>',
-                                '<tr><td>{[this.getWhatYouNeedToKnowWiki()]}</td></tr>',
-                                '</table></div>',
-                                    '<div id="helpCenter"><table>',
-                                    '<tr><td style="padding-top: 10px;">See the <a id="helpCenterDialog" class="helpcenter-panel" href="#" onclick="return false">Help</a> section for more info</td></tr>',
-                                '</table></div>',
-                            '</td>',
-                        '</tr>',
-                        '</table>',
+            '<h1 class="started-section-title bottom-spacer">Quick Links</h1>',
+            '<a id="hidelink" href="#" onclick="return false;" class="started-dismiss">Hide &nbsp; <span style="font-size: 9px">&#x2715;</span></a>',
+            '<tpl if="videoURL">',
+                '<div style="height:auto" class="get-started ',
+            '<tpl if="multiRow">',
+                ' get-started-multiRow"',
+            '<tpl else>',
+                ' get-started-singleRow"',
+            '</tpl>',
+            '>',
 
-                    '<tpl elif="isAdmin">',
-                    '<div style="margin-left: 28px;">Hey! You look like an admin. The Get Started Video URL needs to be setup.&nbsp;',
-                    '<a href="{adminURL}" target="_blank">Configure</a></div>',
-                    '</tpl>', {
-                        getWhatYouNeedToKnowWiki: function() {
-                            return new LABKEY.WebPart({
-                                partName: 'Wiki',
-                                frame: 'none',
-                                renderTo: 'whatyouneedtoknow-wiki',
-                                containerPath: '/Shared',
-                                partConfig: {
-                                    name: LABKEY.getModuleProperty('cds', 'WhatYouNeedToKnowWiki')
-                                }
-                            }).render();
-                        },
-                        getTakeATourWiki: function() {
-                            return new LABKEY.WebPart({
-                                partName: 'Wiki',
-                                frame: 'none',
-                                renderTo: 'tours-wiki',
-                                containerPath: '/Shared',
-                                partConfig: {
-                                    name: LABKEY.getModuleProperty('cds', 'ToursWiki')
-                                }
-                            }).render();
+            '<table style="table-layout: fixed;" class="tile-group"><tr>',
+                '<td style="vertical-align: top"><div>',
+                    '<table class="tile" id="home-video">',
+                        '<tr><td class="tile-image"><div class="home_video backgroundimage"></div></td></tr>',
+                        '<tr><td><h3 class="tile-title" style="white-space: nowrap;">Watch the <a href="{videoURL}" target="_blank">Get Started</a> video</h3></td></td></tr>',
+                        '<tr><td>See the most powerful ways to explore the DataSpace.</td></tr>',
+                    '</table>',
+                '</div></td>',
+
+                '<td style="vertical-align: top"><div>',
+                    '<table class="tile">',
+                        '<tr><td class="tile-image"><div class="home_bar backgroundimage"></div></td></tr>',
+                        '<tr><td style="padding-left:20%;"><div id="tours-wiki">{[this.getTakeATourWiki()]}</div></td></tr>',
+                    '</table>',
+                '</div></td>',
+
+                '<tpl if="multiRow">',
+                    '</tr><tr>',
+                '</tpl>',
+
+                '<td style="vertical-align: top;"><div>',
+                    '<table class="tile">',
+                        '<tr><td class="tile-image"><div class="home_try_it_out backgroundimage"></div></td></tr>',
+                        '<tr><td style="padding-left: 25%"><div>',
+                            '<h3 class="tile-title">Try it out</h3>',
+                            '<ul>',
+                                '<li style="padding-bottom:10px"><a href=\'#learn/learn/Study/\' target="_blank">Learn about</a> studies, products, assays, antibodies, ad publications</li>',
+                                '<li style="padding-bottom:10px"><a href=\'#summary\' target="_blank">Find subjects</a> with common characteristics</li>',
+                                '<li style="padding-bottom:10px"><a href=\'#chart\' target="_blank">Plot</a> assay results across studies and years of research</li>',
+                                '<li>Compare <a href=\'#mabgrid\' target="_blank">Monoclonal antibodies</a> and their neutralization curves</li>',
+                            '</ul>',
+                        '</div></td></tr>',
+                    '</table>',
+                '</div></td>',
+
+                '<td style="vertical-align: top"><div>',
+                    '<table class="tile">',
+                        '<tr><td class="tile-image"><div class="home_integrated_data backgroundimage"></div></td></tr>',
+                        '<tr><td style="padding-left:20%;white-space: normal"><div id="whatyouneedtoknow-wiki">{[this.getWhatYouNeedToKnowWiki()]}</div></td></tr>',
+                        '<tr><td style="padding-top: 5%;padding-left:20%;"><div id="helpCenter">See the <a id="helpCenterDialog" class="helpcenter-panel" href="#" onclick="return false">Help</a> section for more info</div></td></tr>',
+                    '</table>',
+                '</div></td>',
+
+            '</tr></table>',
+
+            '<tpl elif="isAdmin">',
+            '<div style="margin-left: 28px;">Hey! You look like an admin. The Get Started Video URL needs to be setup.&nbsp;',
+            '<a href="{adminURL}" target="_blank">Configure</a></div>',
+            '</tpl>',{
+                getWhatYouNeedToKnowWiki: function() {
+                    return new LABKEY.WebPart({
+                        partName: 'Wiki',
+                        frame: 'none',
+                        renderTo: 'whatyouneedtoknow-wiki',
+                        containerPath: '/Shared',
+                        partConfig: {
+                            name: LABKEY.getModuleProperty('cds', 'WhatYouNeedToKnowWiki')
                         }
-                    },
+                    }).render();
+                },
+                getTakeATourWiki: function() {
+                    return new LABKEY.WebPart({
+                        partName: 'Wiki',
+                        frame: 'none',
+                        renderTo: 'tours-wiki',
+                        containerPath: '/Shared',
+                        partConfig: {
+                            name: LABKEY.getModuleProperty('cds', 'ToursWiki')
+                        }
+                    }).render();
+                }
+            },
             '</div></div>',
             '<a id="showlink" href="#" onclick="return false;" class="started-show"',
-                '<tpl if="showIntro==true">',
-                ' style="display: none;"',
-                '</tpl>',
-                '>Show tips for getting started  &#709;',
+            '<tpl if="showIntro==true">',
+            ' style="display: none;"',
+            '</tpl>',
+            '>Show tips for getting started  &#709;',
             '</a>'
     ),
 
@@ -161,10 +176,10 @@ Ext.define('Connector.component.Started', {
         {
             var divHeight = Ext.get('expanded-intro-div').getHeight();
             var divWidth = c.getEl().dom.offsetWidth;
-            var needResize = (divWidth < 880 && divHeight < 600) || (divWidth > 880 && divHeight > 600);
+            var needResize = (divWidth < 880 && divHeight < 800) || (divWidth > 880 && divHeight > 800);
             if (needResize) {
                 var isMulti = false;
-                if (divWidth < 880 && divHeight < 600) {
+                if (divWidth < 880 && divHeight < 800) {
                     isMulti = true;
                 }
                 var data = {
