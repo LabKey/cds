5ccf5fb872ba036faa8d1ed3ea1661610e0f94c4
Binal <binalp@labkey.com>
2019-10-28 17:48:29 -0700

    Implement show all for Publications section. Plus sample data.

--- webapp/Connector/src/app/view/module/StudyPublications.js
+++ webapp/Connector/src/app/view/module/StudyPublications.js
@@ -7,40 +7,77 @@ Ext.define('Connector.view.module.StudyPublications', {
 
     xtype : 'app.module.studypublications',
 
-    extend : 'Connector.view.module.BaseModule',
+    extend : 'Connector.view.module.ShowList',
 
     plugins : ['documentvalidation'],
 
     tpl : new Ext.XTemplate(
-            '<tpl if="publications && publications.length &gt; 0">',
-                '<h3>{title_study_publications}</h3>',
-                '<table class="learn-study-info">',
-                    '<tpl for="publications">',
-                        '<tr>',
-                            '<td class="item-value">{authors:htmlEncode}. {title:htmlEncode}. {journal:htmlEncode}. {date:htmlEncode};{volume}',
-                            '<tpl if="issue">',
-                                '({issue:htmlEncode})',
+            '<tpl>',
+                '<tpl if="publications && publications.length &gt; 0">',
+                    '<h3  id="pub_listing_title" class="listing_title">{title_study_publications}</h3>',
+                    '<table class="learn-study-info">',
+                        '<tpl for="publications">',
+                            '<tpl if="xindex &lt; 11">',
+                                '<tr>',
+                                    '<td class="item-value">{authors:htmlEncode}. {title:htmlEncode}. {journal:htmlEncode}. {date:htmlEncode};{volume}',
+                                    '<tpl if="issue">',
+                                        '({issue:htmlEncode})',
+                                    '</tpl>',
+                                    ':{location:htmlEncode}. ',
+                                    '<tpl if="pmid">',
+                                        'PMID: {pmid:htmlEncode}. ',
+                                    '</tpl>',
+                                    '<tpl if="link">',
+                                            '<a href="{link}" target="_blank">View publication <img src="' + LABKEY.contextPath + '/Connector/images/outsidelink.png' + '"/></a></td>',
+                                    '</tpl>',
+                                '</tr>',
                             '</tpl>',
-                            ':{location:htmlEncode}. ',
-                            '<tpl if="pmid">',
-                                'PMID: {pmid:htmlEncode}. ',
-                            '</tpl>',
-                            '<tpl if="link">',
-                                    '<a href="{link}" target="_blank">View publication <img src="' + LABKEY.contextPath + '/Connector/images/outsidelink.png' + '"/></a></td>',
-                            '</tpl>',
-                        '</tr>',
+                        '</tpl>',
+                    '</table>',
+
+
+                    '<tpl if="publications.length &gt; 10">',
+                        'and {publications.length - 10} more ',
+                        '<tpl if="showAll">',
+                            '<span class="show-hide-toggle-pub">(show less)</span>',
+                        '<tpl else>',
+                            '<span class="show-hide-toggle-pub">(show all)</span>',
+                        '</tpl>',
+                        '</br></br>',
                     '</tpl>',
-                '</table>',
+
+                    '<tpl if="publications.length &gt; 10">',
+                        '<table class="learn-study-info">',
+                            '<tpl for="publications">',
+                                '<tpl if="parent.showAll && (xindex &gt; 10)">',
+                                    '<tr>',
+                                        '<td class="item-value">{authors:htmlEncode}. {title:htmlEncode}. {journal:htmlEncode}. {date:htmlEncode};{volume}',
+                                        '<tpl if="issue">',
+                                            '({issue:htmlEncode})',
+                                        '</tpl>',
+                                            ':{location:htmlEncode}. ',
+                                        '<tpl if="pmid">',
+                                            'PMID: {pmid:htmlEncode}. ',
+                                        '</tpl>',
+                                        '<tpl if="link">',
+                                            '<a href="{link}" target="_blank">View publication <img src="' + LABKEY.contextPath + '/Connector/images/outsidelink.png' + '"/></a></td>',
+                                        '</tpl>',
+                                    '</tr>',
+                                '</tpl>',
+                            '</tpl>',
+                        '</table>',
+
+                '</tpl>',
             '</tpl>'
     ),
 
     initComponent : function() {
-        this.callParent();
-
-        var data = this.initialConfig.data.model.data;
+        var data = this.getListData();
         data['title_study_publications'] = this.initialConfig.data.title;
-
+        data['showAll'] = false;
         this.update(data);
+
+        this.callParent();
     },
 
     hasContent : function() {
@@ -49,5 +86,17 @@ Ext.define('Connector.view.module.StudyPublications', {
             return pubs.length > 0;
         }
         return false;
+    },
+
+    getListData : function () {
+        return this.initialConfig.data.model.data;
+    },
+
+    scrollListIntoView: function() {
+        Ext.get('pub_listing_title').el.dom.scrollIntoView();
+    },
+
+    getToggleId : function () {
+        return Ext.query('.show-hide-toggle-pub');
     }
 });
