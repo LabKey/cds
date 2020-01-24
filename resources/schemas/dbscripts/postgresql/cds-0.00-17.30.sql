5b81a4a8ae9ab4619aa26af395b3118def0b66e6
Adam Rauch <adam@labkey.com>
2020-01-24 15:33:54 -0800

    Earliest upgrade is 17.30, so merge all 17.20-17.30 scripts into the bootstrap scripts

--- resources/schemas/dbscripts/postgresql/cds-0.00-17.30.sql
+++ resources/schemas/dbscripts/postgresql/cds-0.00-17.30.sql
@@ -1043,3 +1043,22 @@ ALTER TABLE cds.import_nab ADD COLUMN slope NUMERIC(15,4);
 -- mfi_bkgd, auc --
 ALTER TABLE cds.import_bama ADD COLUMN mfi_bkgd NUMERIC(15, 4);
 ALTER TABLE cds.import_bama ADD COLUMN auc NUMERIC(15, 4);
+
+/* cds-17.20-17.30.sql */
+
+ALTER TABLE cds.import_nab DROP COLUMN titer_response_ID50;
+ALTER TABLE cds.import_nab DROP COLUMN titer_response_ID80;
+ALTER TABLE cds.import_nab ADD COLUMN nab_response_ID50 BOOLEAN;
+ALTER TABLE cds.import_nab ADD COLUMN nab_response_ID80 BOOLEAN;
+
+ALTER TABLE cds.import_ics ADD COLUMN response_method VARCHAR(250);
+ALTER TABLE cds.import_ics ADD COLUMN control BOOLEAN;
+ALTER TABLE cds.import_ics ADD COLUMN pooled_info VARCHAR(250);
+
+ALTER TABLE cds.import_bama ADD COLUMN mfi_bkgd_blank NUMERIC(15, 4);
+
+ALTER TABLE cds.import_studyassay ADD COLUMN provenance_source VARCHAR(250);
+ALTER TABLE cds.import_studyassay ADD COLUMN provenance_summary TEXT;
+
+ALTER TABLE cds.studyassay ADD COLUMN provenance_source VARCHAR(250);
+ALTER TABLE cds.studyassay ADD COLUMN provenance_summary TEXT;
