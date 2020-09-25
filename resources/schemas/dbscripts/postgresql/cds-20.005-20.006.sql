6fa33c6bbdd82b3c203fb837b5170c64e307ff47
Binal <binalp@labkey.com>
2020-09-25 13:49:25 -0700

    Drop NOT NULL constraint on target_cell column in cds.import_nabantigen and cds.nabantigen.

--- resources/schemas/dbscripts/postgresql/cds-20.005-20.006.sql
+++ resources/schemas/dbscripts/postgresql/cds-20.005-20.006.sql
@@ -1,2 +1,7 @@
+-- add target_cell columns back since data for target_cell should be coming from nab and nabmab assays, and not nabantigen metadata table
 ALTER TABLE cds.import_nab ADD COLUMN target_cell VARCHAR(250);
 ALTER TABLE cds.import_nabmab ADD COLUMN target_cell VARCHAR(250);
+
+-- Drop NOT NULL constraint since target_cell column is now expected to have null vals in nabantigen metadata table
+ALTER TABLE cds.import_nabantigen ALTER COLUMN target_cell DROP NOT NULL;
+ALTER TABLE cds.nabantigen ALTER COLUMN target_cell DROP NOT NULL;
