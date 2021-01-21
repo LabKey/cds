6821f3ea550dd80ccfab987b9685b2acc9235569
Binal <binalp@labkey.com>
2021-01-21 12:13:20 -0800

    Automated test fixes in CDSLoginTest: - pretest() - test timing out in beginAt fn before reaching getDriver().navigate().to() - so using it directly. - Logging out no longer shows a popup with "Your session has timed out. Please login to continue." - it directly takes the user to CDS login page.

--- test/src/org/labkey/test/tests/cds/CDSLoginTest.java
+++ test/src/org/labkey/test/tests/cds/CDSLoginTest.java
@@ -40,7 +40,7 @@ public class CDSLoginTest extends CDSReadOnlyTest
         signOut();
         Map<String, String> loginParams = new HashMap<>();
         loginParams.put("login", "true");
-        beginAt(WebTestHelper.buildURL("cds", getProjectName(), "app", loginParams));
+        getDriver().navigate().to(WebTestHelper.buildURL("cds", getProjectName(), "app", loginParams));
     }
 
     @Test
@@ -74,10 +74,10 @@ public class CDSLoginTest extends CDSReadOnlyTest
         // see the grid navigation link and clicking on it will generate a time out message.
         if (isElementPresent(CDSHelper.NavigationLink.GRID.getLinkLocator()) && isElementVisible(CDSHelper.NavigationLink.GRID.getLinkLocator()))
         {
-            clickAndWait(CDSHelper.NavigationLink.GRID.getLinkLocator());
+            click(CDSHelper.NavigationLink.GRID.getLinkLocator());
         }
 
-        waitForElement(Locator.css("p").withText("Your session has timed out. Please login to continue."));
+        waitForElement(Locator.tag("h1").withText("CAVD DataSpace member sign-in"));
     }
 
     @Override
