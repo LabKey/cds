2db148295cfaf869717eeb2ebb0b1319893aa69a
Binal Patel <binalp@labkey.com>
2021-02-28 19:16:52 -0800

    Secure Issue 41464: Dataspace - Issues scrolling pages accessed via Learn in Firefox - removal of extra auto-scroll-y is no longer needed, looks like firefox is handling it correctly now.

--- webapp/Connector/src/app/view/AssayAntigen.js
+++ webapp/Connector/src/app/view/AssayAntigen.js
@@ -37,9 +37,6 @@ Ext.define('Connector.view.AssayAntigen', {
         if (this.learnViewConfig)
         {
             this.learnView = this.learnViewConfig.learnView;
-            if (this.learnView.getEl().contains('auto-scroll-y') || this.learnViewConfig.learnView.getEl().hasCls('auto-scroll-y')) {
-                this.learnView.getEl().removeCls('auto-scroll-y');
-            }
             this.tabId = this.learnViewConfig.tabId;
             this.tabDimension = this.learnViewConfig.tabDimension;
             this.tabParams = this.learnViewConfig.tabParams;
