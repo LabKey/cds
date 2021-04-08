aeadb21298fca7d84dbd806f4125fec87cf9d33f
Alan Vezina <alanv@labkey.com>
2021-04-07 19:04:16 -0500

    Copy images used in css to resources/web/cds/gen/images

--- theme/build/connector.config.js
+++ theme/build/connector.config.js
@@ -1,8 +1,24 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 const styleConfig = require('./shared');
+
+const imagesDir = path.resolve(__dirname, '../../webapp/production/Connector/resources/images/');
 
 module.exports = {
     ...styleConfig,
     entry: path.resolve(__dirname, '../connector/theme.scss'),
-    plugins: styleConfig.plugins('Connector-all.css'),
+    plugins: [
+        ...styleConfig.plugins('Connector-all.css'),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "../../webapp/production/Connector/resources/images/**/*",
+                    to({ context, absoluteFilename }) {
+                        const imageDir = absoluteFilename.replace(imagesDir, '');
+                        return `images${imageDir}`;
+                    },
+                }
+            ]
+        }),
+    ],
 };
