3ff65b89906f67384958afb6d1be84d54779beac
Binal Patel <binalp@labkey.com>
2021-04-17 20:27:38 -0700

    Expand groups of 10 or more.

--- webapp/Connector/src/app/view/module/TreatmentSchemaGroup.js
+++ webapp/Connector/src/app/view/module/TreatmentSchemaGroup.js
@@ -9,11 +9,13 @@ Ext.define('Connector.view.module.TreatmentSchemaGroup', {
 
     plugins : ['documentvalidation'],
 
-    extend: 'Ext.container.Container',
+    extend: 'Connector.view.module.ShowList',
+
+    showAll: false,
 
     tpl : new Ext.XTemplate(
         '<tpl>',
-            '<tpl if="groups || treatment_schema_link">',
+            '<tpl if="groups_data || treatment_schema_link">',
                 '<div class="module">',
                     '<h3 id="treatment_groups_title" class="listing_title">{group_title}</h3>',
                         '<tpl if="treatment_schema_link_valid">',
@@ -21,17 +23,45 @@ Ext.define('Connector.view.module.TreatmentSchemaGroup', {
                                 '<a id="groups_treatment_link" href= "{treatment_schema_link_1}" target="_blank">Click for treatment schema</a>',
                             '</div>',
                         '</tpl>',
-                        '{groups}',
+                        '{groups_header}',
+                        '<ul class="learn-study-info"><tbody>',
+                            '<tpl for="groups_data">',
+                                '<tpl if="xindex &lt; 11">',
+                                    '<tpl if="label">',
+                                        '<li class="item-value" style="padding-right: 1em;">{label:htmlEncode}</li>',
+                                    '</tpl>',
+                                '</tpl>',
+                            '</tpl>',
+                        '</ul>',
+                        '<tpl if="groups_data.length &gt; 10">',
+                        'and {groups_data.length - 10} more ',
+                            '<tpl if="showAll">',
+                                '<span class="show-hide-toggle-groups">(show less)</span>',
+                            '<tpl else>',
+                                '<span class="show-hide-toggle-groups">(show all)</span>',
+                            '</tpl>',
+                            '</br></br>',
+                        '</tpl>',
+                        '<ul class="learn-study-info">',
+                            '<tpl for="groups_data">',
+                                '<tpl if="parent.showAll && (xindex &gt; 10)">',
+                                    '<tpl if="label">',
+                                        '<li class="item-value" style="padding-right: 1em;">{label:htmlEncode}</li>',
+                                    '</tpl>',
+                                '</tpl>',
+                            '</tpl>',
+                        '</ul>',
                 '</div>',
             '</tpl>',
         '</tpl>'
     ),
 
     initComponent : function() {
-        this.callParent();
 
-        var data = this.initialConfig.data.model.data;
+        var data = this.getListData();
         data['group_title'] = "Groups";
+        data['showAll'] = this.showAll;
+        this.update(data);
 
         var treatmentSchemaLinkIsValid = function (schema_link, result) {
             data['treatment_schema_link_valid'] = result;
@@ -42,5 +72,27 @@ Ext.define('Connector.view.module.TreatmentSchemaGroup', {
         this.on("afterrender", function () {
             this.validateSchemaAccessLink(data.treatment_schema_link, treatmentSchemaLinkIsValid);
         });
+
+        this.callParent();
+    },
+
+    hasContent : function() {
+        var grps = this.initialConfig.data.model.get('groups_data');
+        if (grps) {
+            return grps.length > 0;
+        }
+        return false;
+    },
+
+    getListData: function () {
+        return this.initialConfig.data.model.data;
+    },
+
+    scrollListIntoView: function() {
+        Ext.get('treatment_groups_title').el.dom.scrollIntoView();
+    },
+
+    getToggleId : function () {
+        return Ext.query('.show-hide-toggle-groups');
     }
 });
