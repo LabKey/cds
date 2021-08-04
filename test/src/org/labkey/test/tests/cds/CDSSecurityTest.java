98fc19d3196ed0599f9f0d493d25e050995a982f
Binal Patel <binalp@labkey.com>
2021-08-04 10:31:02 -0700

    Auto test fix - use search field to get expected element

--- test/src/org/labkey/test/tests/cds/CDSSecurityTest.java
+++ test/src/org/labkey/test/tests/cds/CDSSecurityTest.java
@@ -162,6 +162,8 @@ public class CDSSecurityTest extends CDSReadOnlyTest
     {
         final String NOT_ACCESSIBLE_ICON = "ni-restricted.svg";
         final String HAS_NO_DATA_ICON = "ni-notAdded.svg";
+        String study = "ZAP 138";
+        LearnGrid learnGrid = new LearnGrid(this);
 
         Map<String, String> studyPermissions = new HashMap<>();
         log("Create a user group with Read permission to project but no permission to any study folder");
@@ -169,20 +171,18 @@ public class CDSSecurityTest extends CDSReadOnlyTest
         impersonateGroup(_permGroups[0], false);
         cds.enterApplication();
         cds.viewLearnAboutPage("Studies");
+        learnGrid.setSearch(study);
 
         log("Verify gray checkmark is present indicating limited access");
-        waitForElement(Locator.tagWithClassContaining("div", "detail-has-data-ni-gray"));
+        assertElementPresent(Locator.tagWithClassContaining("div", "detail-has-data-ni-gray"));
 
         log("Verify restricted Non-Integrated Assay is displayed on Learn About > Studies page");
-        LearnGrid learnGrid = new LearnGrid(this);
         int dataAddedColumn = learnGrid.getColumnIndex("Data Added");
         String zap138DataAddedText = "0/1 Non-integrated Assay\n1 Publication";
-        String cellText = learnGrid.getCellText(52, dataAddedColumn);
+        String cellText = learnGrid.getCellText(0, dataAddedColumn);
         Assert.assertTrue("Data Added' column text for study 'ZAP 138' not as expected. Expected: '" + zap138DataAddedText + "'. Found: '" + cellText + "'.",  cellText.trim().toLowerCase().contains(zap138DataAddedText.trim().toLowerCase()));
 
         log("Verify restricted Non-Integrated Assay on Studies page");
-        String study = "ZAP 138";
-        learnGrid.setSearch(study);
         learnGrid.clickFirstItem();
         waitForElement(cds.getDataRowXPath("ILLUMINA 454-X").append("//td//img[contains(@src, '" + NOT_ACCESSIBLE_ICON + "')]"));
         assertElementPresent(Locator.xpath("//td//img[contains(@src, '" + HAS_NO_DATA_ICON + "')]"));
