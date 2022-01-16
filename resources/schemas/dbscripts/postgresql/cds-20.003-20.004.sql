51a8b007e712ba0a61d44a99be4a13b36b5083c0
Adam Rauch <adam@labkey.com>
2022-01-16 11:58:21 -0800

    Consolidate bootstrap scripts to 0.000-21.000.sql (#439)
    Co-authored-by: labkey-klum <klum@labkey.com>

--- resources/schemas/dbscripts/postgresql/cds-20.003-20.004.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE cds.import_nabantigen DROP CONSTRAINT import_nabantigen_pkey;
-ALTER TABLE cds.import_nabantigen ADD PRIMARY KEY (container, assay_identifier, cds_virus_id);
-
-ALTER TABLE cds.nabantigen DROP CONSTRAINT nabantigen_pkey;
-ALTER TABLE cds.nabantigen ADD PRIMARY KEY (container, assay_identifier, cds_virus_id);
