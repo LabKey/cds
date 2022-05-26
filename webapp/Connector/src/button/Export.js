f79e1b85118ebaad12785f71832883152c04c744
Lum <klum@labkey.com>
2022-05-26 16:38:49 -0700

    data export button to consolidate the various export format types refactor grid and MAb export options to use the new component

---
+++ webapp/Connector/src/button/Export.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2008-2022 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+Ext.define('Connector.button.ExportButton', {
+
+    extend: 'Ext.button.Button',
+
+    alias: 'widget.exportbutton',
+
+    text : 'Export',
+
+    arrowCls : 'arrow-light',
+
+    initComponent : function() {
+
+        this.menu = {
+            bubbleEvents : ['click'],
+            defaults : {
+                cls : 'export-item',
+                iconCls: 'export-item-icon',
+            },
+            items : [{
+                text: 'Comma separated values (*.CSV)',
+                itemId : 'csv-menu-item'
+            },{
+                text: 'Excel (*.XLS)',
+                itemId : 'excel-menu-item'
+            }]
+        };
+
+        this.on('click', this.handleClick, this);
+        this.callParent();
+    },
+
+    handleClick : function(cmp, item) {
+        if (item.itemId) {
+            switch (item.itemId) {
+                case 'csv-menu-item' :
+                    this.fireEvent('exportcsv', item);
+                    break;
+                case 'excel-menu-item' :
+                    this.fireEvent('exportexcel', item);
+                    break;
+            }
+        }
+    }
+});
