6c6762969e8f875a108caafab9fc134fc9d8a04d
Binal Patel <binalp@labkey.com>
2023-04-26 14:55:06 -0700

    [Dataspace] Add support to import and store B-Cell Receptor (BCR) data - Client Requested Changes (#573)
    - Updates to BCR tables
    - Sample data updates
    - ETL updates
    - Add alignment and seq. germline queries

--- src/org/labkey/cds/CDSModule.java
+++ src/org/labkey/cds/CDSModule.java
@@ -177,7 +177,7 @@ public class CDSModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 23.002;
+        return 23.003;
     }
 
     @Override
