964f6936698cb604572dabd9760d09bc518a13c1
labkey-sweta <swetaj@labkey.com>
2024-12-23 23:45:52 -0800

    Test updates

--- test/src/org/labkey/test/tests/cds/CDSTestLearnAbout.java
+++ test/src/org/labkey/test/tests/cds/CDSTestLearnAbout.java
@@ -1993,7 +1993,7 @@ public class CDSTestLearnAbout extends CDSReadOnlyTest
         pdfFileName = pdfFileName.toLowerCase();
 
         cds.viewLearnAboutPage(LearnTab.STUDIES);
-
+        sleep(CDSHelper.CDS_WAIT_LEARN);
         log("Validate that study " + studyName + " has a grant document and is of type pdf.");
         goToDetail(studyName, false);
         waitForText(STUDY_INFO_TEXT_TRIGGER);
